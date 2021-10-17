a8db1d8cd22f5445eb5733addd8063bbc6cfa54f
robbychristian <robbychristiandeleon@gmail.com>
2021-10-18 07:10:54 +0800

    Update register.blade.php

--- resources/views/dashboard/user/register.blade.php
+++ resources/views/dashboard/user/register.blade.php
@@ -70,9 +70,6 @@
                                     </select>
                                 </div>
                                 <div class="form-group col-md-2">
-                                    <div class="d-none d-xl-block d-lg-block d-md-block">
-                                        <label for="inputBday" style="color:white"></label>
-                                    </div>
                                     <select name="mbday" id="inputBday" class="form-control">
                                         <option selected>Month</option>
                                         <option value='1'>January</option>
