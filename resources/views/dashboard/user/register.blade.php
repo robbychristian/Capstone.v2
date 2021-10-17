42cc761ced20cfc4bad6db739b477ae92e1117d7
robbychristian <robbychristiandeleon@gmail.com>
2021-10-18 07:17:58 +0800

    Update register.blade.php

--- resources/views/dashboard/user/register.blade.php
+++ resources/views/dashboard/user/register.blade.php
@@ -70,7 +70,7 @@
                                     </select>
                                 </div>
                                 <div class="form-group col-md-2">
-                                    <div class="d-none d-xl-block d-lg-block d-md-block">
+                                    <div class="d-none d-xl-block d-lg-block d-md-block mt-3">
                                         <label for="inputBday" style="color:white">asdasd</label>
                                     </div>
                                     <select name="mbday" id="inputBday" class="form-control">
