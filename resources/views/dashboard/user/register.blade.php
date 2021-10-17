235b3752d0734cee3b130528a4797a9cee2aeea6
robbychristian <robbychristiandeleon@gmail.com>
2021-10-18 07:12:04 +0800

    Update register.blade.php

--- resources/views/dashboard/user/register.blade.php
+++ resources/views/dashboard/user/register.blade.php
@@ -69,6 +69,24 @@
                                         <option value='12'>December</option>
                                     </select>
                                 </div>
+                                <div class="form-group col-md-2">
+                                    <label for="inputBday">Birthday</label>
+                                    <select name="mbday" id="inputBday" class="form-control">
+                                        <option selected>Month</option>
+                                        <option value='1'>January</option>
+                                        <option value='2'>February</option>
+                                        <option value='3'>March</option>
+                                        <option value='4'>April</option>
+                                        <option value='5'>May</option>
+                                        <option value='6'>June</option>
+                                        <option value='7'>July</option>
+                                        <option value='8'>August</option>
+                                        <option value='9'>September</option>
+                                        <option value='10'>October</option>
+                                        <option value='11'>November</option>
+                                        <option value='12'>December</option>
+                                    </select>
+                                </div>
                             </div>
 
                             <div class="form-group">
