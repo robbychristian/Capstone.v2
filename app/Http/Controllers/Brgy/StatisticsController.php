fc2d36d7bba79838309549a2cd8ce4bdce8f31d6
robbychristian <robbychristiandeleon@gmail.com>
2021-10-19 03:15:37 +0800

    Update StatisticsController.php

--- app/Http/Controllers/Brgy/StatisticsController.php
+++ app/Http/Controllers/Brgy/StatisticsController.php
@@ -24,7 +24,7 @@ class StatisticsController extends Controller
      */
     public function create()
     {
-        //
+        return view('features.createdisasterstatsreport');
     }
 
     /**
