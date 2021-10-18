c258b4ff15b8f49f043c22f7b46673af5be04708
robbychristian <robbychristiandeleon@gmail.com>
2021-10-19 02:58:49 +0800

    Update home.blade.php

--- resources/views/dashboard/brgy_official/home.blade.php
+++ resources/views/dashboard/brgy_official/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="container-fluid mt-5">
+    <div class="container-fluid mt-3">
         @if (Auth::user()->brgy_loc == "Barangay Santolan")
             <div class="row">
                 
