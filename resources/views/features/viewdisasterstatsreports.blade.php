5b51687adc1c1dcf9aa3a24115c2d2970f392161
robbychristian <robbychristiandeleon@gmail.com>
2021-10-19 03:27:19 +0800

    Update viewdisasterstatsreports.blade.php

--- resources/views/features/viewdisasterstatsreports.blade.php
+++ resources/views/features/viewdisasterstatsreports.blade.php
@@ -9,16 +9,22 @@
 
             </div>
 
+            @if (Auth::user()->user_role === 3 || Auth::user()->user_role === 1)
+                <!--ACCESSIBLE ONLY TO ADMIN/BRGY-->
+                <div class="col-sm-12 col-md-4">
+                    <div class="row"></div>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        @if (Auth::user()->user_role === 1)
+                            <a href="#" class="btn btn-primary" role="button">Create a
+                                Disaster Statistical Reports</a>
 
-            <!--ACCESSIBLE ONLY TO ADMIN/BRGY-->
-            <div class="col-sm-12 col-md-4">
-                <div class="row"></div>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-
-                    <a href="{{ route('brgy_official.stats.create') }}" class="btn btn-primary" 
-                        role="button">Create a Disaster Statistical Reports</a>
+                        @elseif(Auth::user()->user_role === 3)
+                            <a href="{{ route('brgy_official.stats.create') }}"class="btn btn-primary" role="button">Create a
+                                Disaster Statistical Reports</a>
+                        @endif
+                    </div>
                 </div>
-            </div>
+            @endif
 
         </div>
 
