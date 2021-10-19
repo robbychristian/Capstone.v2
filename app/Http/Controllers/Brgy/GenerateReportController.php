5ce1c71de157fd23333866a5c281be21c0da5d95
robbychristian <robbychristiandeleon@gmail.com>
2021-10-19 23:09:55 +0800

    Update GenerateReportController.php

--- app/Http/Controllers/Brgy/GenerateReportController.php
+++ app/Http/Controllers/Brgy/GenerateReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Brgy;
 use App\Exports\DisasterExport;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Facades\Excel;
 
 
