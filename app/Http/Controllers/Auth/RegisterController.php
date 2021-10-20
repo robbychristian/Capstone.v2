d5da0a80a84f89fdc16d2f091b9f55807d4556f2
robbychristian <robbychristiandeleon@gmail.com>
2021-10-21 03:11:15 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\data;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
@@ -44,7 +44,7 @@ class RegisterController extends Controller
     }
 
     /**
-     * Get a validator for an incoming registration request.
+     * Get a validator for an incoming registration data.
      *
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
@@ -64,9 +64,9 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\Models\User
      */
-    protected function create(Request $request)
+    protected function create(array $data)
     {
-        $validator = Validator::make($request->all(), [
+        $validator = Validator::make($data, [
             'email' => 'required|unique:users|email',
             'fname' => 'required|max:255',
             'mname' => 'required|max:255',
@@ -106,21 +106,21 @@ class RegisterController extends Controller
 
             $user = User::create([
                 'user_role' => 4,
-                'email' => $request['email'],
-                'first_name' => $request['fname'],
-                'last_name' => $request['lname'],
-                'brgy_loc' => $request['brgy'],
+                'email' => $data['email'],
+                'first_name' => $data['fname'],
+                'last_name' => $data['lname'],
+                'brgy_loc' => $data['brgy'],
                 'is_blocked' => 0,
                 'is_deactivated' => 0,
-                'password' => Hash::make($request['password']),
+                'password' => Hash::make($data['password']),
             ]);
 
             $user_profile = UserProfile::create([
-                'user_email' => $request['email'],
-                'middle_name' => $request['mname'],
-                'home_add' => $request['home_add'],
-                'contact_no' => $request['cnum'],
-                'birth_day' => $request['mbday'] . '/' . $request['dbday'] . '/' . $request['ybday'],
+                'user_email' => $data['email'],
+                'middle_name' => $data['mname'],
+                'home_add' => $data['home_add'],
+                'contact_no' => $data['cnum'],
+                'birth_day' => $data['mbday'] . '/' . $data['dbday'] . '/' . $data['ybday'],
                 'profile_pic' => 'noimage.jpg'
             ]);
 
