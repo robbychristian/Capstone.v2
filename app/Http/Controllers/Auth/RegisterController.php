f5bdbb512b9d6381c1caedb743e8b176242e70e0
robbychristian <robbychristiandeleon@gmail.com>
2021-10-21 04:15:31 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,21 +52,6 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-    }
-
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Models\User
-     */
-    protected function create(array $data)
-    {
-        $validator = Validator::make($data, [
             'email' => 'required|unique:users|email',
             'fname' => 'required|max:255',
             'mname' => 'required|max:255',
@@ -97,34 +82,68 @@ class RegisterController extends Controller
             'cpass.same' => 'Confirm password should match password!',
             'cbox.accepted' => 'Terms and conditions must be confirmed!',
         ]);
+    }
 
-        if ($validator->fails()) {
-            return redirect('/user/register/')
-                ->withErrors($validator)
-                ->withInput();
-        } else {
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        //$validator = Validator::make($data, [
+        //    'email' => 'required|unique:users|email',
+        //    'fname' => 'required|max:255',
+        //    'mname' => 'required|max:255',
+        //    'lname' => 'required|max:255',
+        //    'home_add' => 'required|max:255',
+        //    'brgy' => 'required',
+        //    'mbday' => 'required',
+        //    'dbday' => 'required',
+        //    'ybday' => 'required',
+        //    'email' => 'required|email|unique:users|string',
+        //    'cnum' => 'required|max:255',
+        //    'pass' => 'required|min:8',
+        //    'cpass' => 'required|min:8|same:pass',
+        //    'cbox' => 'accepted'
+        //], $messages = [
+        //    'fname.required' => 'The first name field must not be empty!',
+        //    'mname.required' => 'The middle name field must not be empty!',
+        //    'lname.required' => 'The last name field must not be empty!',
+        //    'brgy.required' => 'The Barangay field must not be empty!',
+        //    'home_add.required' => 'The Home Address field must not be empty!',
+        //    'mbday.required' => 'The month field must not be empty!',
+        //    'dbday.required' => 'The day field must not be empty!',
+        //    'ybday.required' => 'The year field must not be empty!',
+        //    'email.required' => 'The email field must not be empty!',
+        //    'cnum.required' => 'The contact number field must not be empty!',
+        //    'pass.required' => 'The password field must not be empty!',
+        //    'cpass.required' => 'The confirm password field must not be empty!',
+        //    'cpass.same' => 'Confirm password should match password!',
+        //    'cbox.accepted' => 'Terms and conditions must be confirmed!',
+        //]);
 
-            $user = User::create([
-                'user_role' => 4,
-                'email' => $data['email'],
-                'first_name' => $data['fname'],
-                'last_name' => $data['lname'],
-                'brgy_loc' => $data['brgy'],
-                'is_blocked' => 0,
-                'is_deactivated' => 0,
-                'password' => Hash::make($data['password']),
-            ]);
+        $user = User::create([
+            'user_role' => 4,
+            'email' => $data['email'],
+            'first_name' => $data['fname'],
+            'last_name' => $data['lname'],
+            'brgy_loc' => $data['brgy'],
+            'is_blocked' => 0,
+            'is_deactivated' => 0,
+            'password' => Hash::make($data['password']),
+        ]);
 
-            $user_profile = UserProfile::create([
-                'user_email' => $data['email'],
-                'middle_name' => $data['mname'],
-                'home_add' => $data['home_add'],
-                'contact_no' => $data['cnum'],
-                'birth_day' => $data['mbday'] . '/' . $data['dbday'] . '/' . $data['ybday'],
-                'profile_pic' => 'noimage.jpg'
-            ]);
+        $user_profile = UserProfile::create([
+            'user_email' => $data['email'],
+            'middle_name' => $data['mname'],
+            'home_add' => $data['home_add'],
+            'contact_no' => $data['cnum'],
+            'birth_day' => $data['mbday'] . '/' . $data['dbday'] . '/' . $data['ybday'],
+            'profile_pic' => 'noimage.jpg'
+        ]);
 
-            return $user;
-        }
+        return $user;
     }
 }
