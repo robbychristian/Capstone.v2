4b1f957a2ef5823135c23fdeda5f49ed210d5670
robbychristian <robbychristiandeleon@gmail.com>
2021-10-20 20:25:57 +0800

    update blades

--- resources/views/dashboard/LGU/maybunga.blade.php
+++ resources/views/dashboard/LGU/maybunga.blade.php
@@ -6,9 +6,9 @@
     <div class="col-xl-10 col-lg-9 col-md-8">
 
         <div class="d-grid gap-2  d-lg-flex d-md-flex justify-content-md-end mt-4">
-            <a class="btn btn-primary" href="/lgu/generate" role="button">Generate Report</a>
+            <a class="btn btn-primary" href="/lgu/generate/{{ $barangay }}" role="button">Generate Report</a>
         </div>
-        <h3>Barangay Maybunga</h3>
+        <h3>{{ $barangay }}</h3>
         <div class="card mt-3" style="position: relative; height:50vh;">
             <div class="card-body">
                 <canvas id="myChart"></canvas>
