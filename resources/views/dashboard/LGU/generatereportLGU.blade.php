5225d3ae054cc1bc55db52635bd0875aede6c1b8
robbychristian <robbychristiandeleon@gmail.com>
2021-10-20 19:50:16 +0800

    Update generatereportLGU.blade.php

--- resources/views/dashboard/LGU/generatereportLGU.blade.php
+++ resources/views/dashboard/LGU/generatereportLGU.blade.php
@@ -13,7 +13,7 @@
                         {{ Session::get('error') }}
                     </div>
                 @endif
-                    <h3>{{ $request->route('barangay'); }}</h3>
+                    <h3>{{ $request->route('brgy') }}</h3>
                 <form action="/lgu/generate" method="POST">
                     @csrf
                     <div class="form-row">
