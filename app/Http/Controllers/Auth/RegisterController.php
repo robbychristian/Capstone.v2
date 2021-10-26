2593e133619a35d4f3d9a9b3f6b6098f6ea707bb
robbychristian <robbychristiandeleon@gmail.com>
2021-10-27 04:21:40 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
-    protected function validator(array $data, Request $request)
+    protected function validator(array $data)
     {
 
         return Validator::make($data, [
