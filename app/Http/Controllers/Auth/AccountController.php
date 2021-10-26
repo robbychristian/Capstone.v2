118be2faafe6e4735cb14740b240c49b7995d943
robbychristian <robbychristiandeleon@gmail.com>
2021-10-27 00:11:02 +0800

    Update AccountController.php

--- app/Http/Controllers/Auth/AccountController.php
+++ app/Http/Controllers/Auth/AccountController.php
@@ -94,7 +94,7 @@ class AccountController extends Controller
             'mname.required' => 'The middle name field must not be empty!',
             'lname.required' => 'The last name field must not be empty!',
             'cnum.required' => 'The contact number field must not be empty!',
-            'cnum.integer' => 'Invalid phone number!',
+            'cnum.numeric' => 'Invalid phone number!',
             'cnum.min' => 'The contact number must be 11 digits only!',
             'curr_pass.required' => 'The current password field must not be empty!',
             'new_pass.required' => 'The new password field must not be empty!',
