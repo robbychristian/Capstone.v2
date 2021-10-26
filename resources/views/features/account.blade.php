8bdee71e4f6061d8876ecc8d1edd93616235131d
robbychristian <robbychristiandeleon@gmail.com>
2021-10-27 00:33:37 +0800

    Update account.blade.php

--- resources/views/features/account.blade.php
+++ resources/views/features/account.blade.php
@@ -11,99 +11,116 @@
                     {{ Session::get('success') }}
                 </div>
             @endif
-            
-            <div class="card">
-                <div class="card-body">
-                    @if (Auth::user()->user_role === 4)
-                        <form action="/user/account/{{ $user->id }}" method="POST">
-                    @endif
-                    @csrf
-                    @method('PUT')
-                    <h5 class="mb-3">Account Information</h5>
-                    <div class="form-row">
-                        <div class="form-group col-md-4">
-                            <label for="inputfName">First Name</label>
-                            <input type="text" name="fname" class="form-control @error('fname') is-invalid @enderror"
-                                id="inputfName" value="{{ $user->first_name }}">
-                            <small class="text-danger">@error('fname')
-                                    {{ $message }}
-                                @enderror</small>
+            <div class="row">
+                @if (Auth::user()->user_role === 4)
+                    <form action="/user/account/{{ $user->id }}" method="POST">
+                @endif
+                @csrf
+                @method('PUT')
+                <div class="col">
+                    <div class="card">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-                        <div class="form-group col-md-4">
-                            <label for="inputmName">Middle Name</label>
-                            <input type="text" name="mname" class="form-control @error('mname') is-invalid @enderror"
-                                id="inputmName" value="{{ $profile->middle_name }}">
-                            <small class="text-danger">@error('mname')
-                                    {{ $message }}
-                                @enderror</small>
-                        </div>
+                      </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
 
-                        <div class="form-group col-md-4">
-                            <label for="inputlName">Last Name</label>
-                            <input type="text" name="lname" class="form-control @error('lname') is-invalid @enderror"
-                                id="inputlName" value="{{ $user->last_name }}">
-                            <small class="text-danger">@error('lname')
-                                    {{ $message }}
-                                @enderror</small>
+                            <h5 class="mb-3">Account Information</h5>
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="inputfName">First Name</label>
+                                    <input type="text" name="fname"
+                                        class="form-control @error('fname') is-invalid @enderror" id="inputfName"
+                                        value="{{ $user->first_name }}">
+                                    <small class="text-danger">@error('fname')
+                                            {{ $message }}
+                                        @enderror</small>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="inputmName">Middle Name</label>
+                                    <input type="text" name="mname"
+                                        class="form-control @error('mname') is-invalid @enderror" id="inputmName"
+                                        value="{{ $profile->middle_name }}">
+                                    <small class="text-danger">@error('mname')
+                                            {{ $message }}
+                                        @enderror</small>
+                                </div>
+
+                                <div class="form-group col-md-4">
+                                    <label for="inputlName">Last Name</label>
+                                    <input type="text" name="lname"
+                                        class="form-control @error('lname') is-invalid @enderror" id="inputlName"
+                                        value="{{ $user->last_name }}">
+                                    <small class="text-danger">@error('lname')
+                                            {{ $message }}
+                                        @enderror</small>
+                                </div>
+                            </div>
+
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <label for="inputEmail">Email Address</label>
+                                    <input type="text" name="email"
+                                        class="form-control @error('email') is-invalid @enderror" id="inputEmail"
+                                        value="{{ $user->email }}" readonly>
+                                    <small class="text-danger">@error('email')
+                                            {{ $message }}
+                                        @enderror</small>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="inputContactNum">Contact Number</label>
+                                    <input type="text" name="cnum" class="form-control @error('cnum') is-invalid @enderror"
+                                        id="inputContactNum" value="{{ $profile->contact_no }}">
+                                    <small class="text-danger">@error('cnum')
+                                            {{ $message }}
+                                        @enderror</small>
+                                </div>
+                            </div>
+
+                            <hr>
+                            <h5 class="mb-3">Change your Password</h5>
+
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="inputCurrentPw">Current Password</label>
+                                    <input type="password" name="curr_pass"
+                                        class="form-control @error('curr_pass') is-invalid @enderror" id="inputCurrentPw">
+                                    <small class="text-danger">@error('curr_pass')
+                                            {{ $message }}
+                                        @enderror</small>
+                                </div>
+                                <div class="form-group col-md-4">
+                                    <label for="inputNewPw">New Password</label>
+                                    <input type="password" name="new_pass"
+                                        class="form-control  @error('new_pass') is-invalid @enderror" id="inputNewPw">
+                                    <small class="text-danger">@error('new_pass')
+                                            {{ $message }}
+                                        @enderror</small>
+                                </div>
+
+                                <div class="form-group col-md-4">
+                                    <label for="inputConfPw">Confirm New Password</label>
+                                    <input type="password" name="conf_pass"
+                                        class="form-control @error('conf_pass') is-invalid @enderror" id="inputConfPw">
+                                    <small class="text-danger">@error('conf_pass')
+                                            {{ $message }}
+                                        @enderror</small>
+                                </div>
+                            </div>
+                            <div class=" d-grid gap-2 d-md-block">
+                                <button class="btn btn-primary" type="submit">Submit</button>
+                                <a class="btn btn-outline-secondary" href="#" role="button">Cancel</a>
+                            </div>
+                            </form>
                         </div>
                     </div>
-
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="inputEmail">Email Address</label>
-                            <input type="text" name="email" class="form-control @error('email') is-invalid @enderror"
-                                id="inputEmail" value="{{ $user->email }}" readonly>
-                            <small class="text-danger">@error('email')
-                                    {{ $message }}
-                                @enderror</small>
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="inputContactNum">Contact Number</label>
-                            <input type="text" name="cnum" class="form-control @error('cnum') is-invalid @enderror"
-                                id="inputContactNum" value="{{ $profile->contact_no }}">
-                            <small class="text-danger">@error('cnum')
-                                    {{ $message }}
-                                @enderror</small>
-                        </div>
-                    </div>
-
-                    <hr>
-                    <h5 class="mb-3">Change your Password</h5>
-
-                    <div class="form-row">
-                        <div class="form-group col-md-4">
-                            <label for="inputCurrentPw">Current Password</label>
-                            <input type="password" name="curr_pass"
-                                class="form-control @error('curr_pass') is-invalid @enderror" id="inputCurrentPw">
-                            <small class="text-danger">@error('curr_pass')
-                                    {{ $message }}
-                                @enderror</small>
-                        </div>
-                        <div class="form-group col-md-4">
-                            <label for="inputNewPw">New Password</label>
-                            <input type="password" name="new_pass"
-                                class="form-control  @error('new_pass') is-invalid @enderror" id="inputNewPw">
-                            <small class="text-danger">@error('new_pass')
-                                    {{ $message }}
-                                @enderror</small>
-                        </div>
-
-                        <div class="form-group col-md-4">
-                            <label for="inputConfPw">Confirm New Password</label>
-                            <input type="password" name="conf_pass"
-                                class="form-control @error('conf_pass') is-invalid @enderror" id="inputConfPw">
-                            <small class="text-danger">@error('conf_pass')
-                                    {{ $message }}
-                                @enderror</small>
-                        </div>
-                    </div>
-                    <div class=" d-grid gap-2 d-md-block">
-                        <button class="btn btn-primary" type="submit">Submit</button>
-                        <a class="btn btn-outline-secondary" href="#" role="button">Cancel</a>
-                    </div>
-                    </form>
                 </div>
             </div>
+
         </div>
 
     @endsection
