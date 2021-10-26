c428a6172d8b65ce1600ca5427b04bcf1a045507
robbychristian <robbychristiandeleon@gmail.com>
2021-10-27 00:50:15 +0800

    Update account.blade.php

--- resources/views/features/account.blade.php
+++ resources/views/features/account.blade.php
@@ -23,7 +23,7 @@
                     <div class="card mb-3">
                         <div class="row no-gutters">
                           <div class="col-md-4">
-                            <img src="{{ asset('img/appmockup.png') }}" alt="...">
+                            <img src="{{ asset('img/appmockup.png') }}" height="300" width="300" alt="...">
                           </div>
                           <div class="col-md-8">
                             <div class="card-body">
