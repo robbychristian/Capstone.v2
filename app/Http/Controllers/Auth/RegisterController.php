07b267ee31d47b396b2e289b93a230eb12dc1295
robbychristian <robbychristiandeleon@gmail.com>
2021-10-27 03:00:39 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,7 +65,7 @@ class RegisterController extends Controller
             'cnum' => 'required|max:255',
             'pass' => 'required|min:8',
             'cpass' => 'required|min:8|same:pass',
-            'file' => 'required|mimes: png, jpeg,jpg|dimensions:max|max:500000',
+            'file' => 'required|mimes: png, jpeg,jpg|max:500000',
             'cbox' => 'accepted'
         ], $messages = [
             'fname.required' => 'The first name field must not be empty!',
