e6f9b1a5d48ad8698a993d342c8d22870eea9863
robbychristian <robbychristiandeleon@gmail.com>
2021-10-27 01:37:19 +0800

    Update account.blade.php

--- resources/views/features/account.blade.php
+++ resources/views/features/account.blade.php
@@ -29,12 +29,12 @@
                           </div>
                           <div class="col-md-8 ">
                             <div class="card-body d-flex">
-                                <div class="custom-file justify-content-end">
+                                <div class="custom-file align-self-end">
                                     <input name="file" type="file" class="custom-file-input" id="inputGroupFile04"
                                         aria-describedby="inputGroupFileAddon04">
                                     <label class="custom-file-label" for="inputGroupFile04">Upload an image</label>
                                 </div>
-                                <p class="card-text justify-content-end"><small class="text-muted">Accessible formats: jpg, png only</small></p>
+                                <p class="card-text align-self-end"><small class="text-muted">Accessible formats: jpg, png only</small></p>
                             </div>
                           </div>
                         </div>
