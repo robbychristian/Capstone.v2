665de3ec63a3ebc9694b036ce7e21e66f7eed5e6
robbychristian <robbychristiandeleon@gmail.com>
2021-10-28 03:50:49 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::get("search/{email}", [AccountController::class, 'search']);
 Route::get("fetch/{email}", [AccountController::class, 'fetchCreds']);
 Route::get("announcements", [AnnouncementController::class, 'fetchAnnouncements']);
 Route::get("fetchreport/{id}", [ReportsController::class, 'fetchReport']);
-Route::post('sendReport', [ReportsController::class, 'sendReport']);
+Route::post('sendreport', [ReportsController::class, 'sendReport']);
 //Route::post('upload/', [ReportsController::class, 'uploadReport']);
 
 Auth::routes();
