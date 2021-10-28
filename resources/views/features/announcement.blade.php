adba151ccd0d62930edf2fccfd3ed9c2e16f4590
robbychristian <robbychristiandeleon@gmail.com>
2021-10-28 11:50:49 +0800

    Update announcement.blade.php

--- resources/views/features/announcement.blade.php
+++ resources/views/features/announcement.blade.php
@@ -39,78 +39,79 @@
         </div>
 
         @empty($announcements)
+
+            <div class="card w-100 card-announce-custom-bg mt-3">
+                <ul class="list-group list-group-flush">
+                    <!-- ANNOUNCEMENT LIST -->
+                    @foreach ($announcements as $announcement)
+                        <li class="list-group-item announcement-list">
+                            <div class="d-flex flex-row align-items-center">
+                                <div class="col-sm-2 col-md-3 col-lg-2 col-xl-1"><i class="fas fa-user-circle fa-4x"
+                                        style="color: #DEDEDE"></i>
+                                </div>
+                                <div class="col-sm-8 col-md-6 col-lg-8 col-xl-9">
+                                    <div class="d-flex flex-column">
+                                        <div class="v-announcement-title"><a href="#">{{ $announcement->title }}</a></div>
+                                        <div class="v-announcement-message">{{ $announcement->body }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-2 col-md-3 col-lg-2 col-xl-2">
+                                    <div class="d-flex flex-column">
+                                        <div class="v-announcement-date-title">Posted on:</div>
+                                        <div class="v-announcement-date">
+                                            {{ Carbon\Carbon::parse($announcement->created_at)->format('d F Y') }}</div>
+                                    </div>
+                                    @if (Auth::user()->user_role === 3 || Auth::user()->user_role === 1)
+                                        @if (Auth::user()->user_role === 1)
+                                            <div class="d-flex flex-row">
+                                                <div class="v-announcement-date-title mr-2">
+                                                    <a href="/admin/announcements/{{ $announcement->id }}/edit">
+                                                        <button class="btn btn-success">Edit</button>
+                                                    </a>
+                                                </div>
+                                                <div class="v-announcement-date">
+                                                    <form action="/admin/announcements/{{ $announcement->id }}" method="POST">
+                                                        @csrf
+                                                        @method('delete')
+                                                        <button class="btn btn-danger">Delete</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        @elseif(Auth::user()->user_role === 3)
+                                            <div class="d-flex flex-row">
+                                                <div class="v-announcement-date-title mr-2">
+                                                    <a href="/brgy_official/announcements/{{ $announcement->id }}/edit">
+                                                        <button class="btn btn-success">Edit</button>
+                                                    </a>
+                                                </div>
+                                                <div class="v-announcement-date">
+                                                    <form action="/brgy_official/announcements/{{ $announcement->id }}"
+                                                        method="POST">
+                                                        @csrf
+                                                        @method('delete')
+                                                        <button class="btn btn-danger">Delete</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        @endif
+                                    @endif
+                                </div>
+                            </div>
+                        </li>
+
+
+                    @endforeach
+
+                    <!-- END OF ANNOUNCEMENT LIST -->
+                </ul>
+            </div>
+        @else
             <div class="card">
                 <div class="card-body">
                     There are no announcements.
                 </div>
             </div>
-        @else
-        <div class="card w-100 card-announce-custom-bg mt-3">
-            <ul class="list-group list-group-flush">
-                <!-- ANNOUNCEMENT LIST -->
-                @foreach ($announcements as $announcement)
-                    <li class="list-group-item announcement-list">
-                        <div class="d-flex flex-row align-items-center">
-                            <div class="col-sm-2 col-md-3 col-lg-2 col-xl-1"><i class="fas fa-user-circle fa-4x"
-                                    style="color: #DEDEDE"></i>
-                            </div>
-                            <div class="col-sm-8 col-md-6 col-lg-8 col-xl-9">
-                                <div class="d-flex flex-column">
-                                    <div class="v-announcement-title"><a href="#">{{ $announcement->title }}</a></div>
-                                    <div class="v-announcement-message">{{ $announcement->body }}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-2 col-md-3 col-lg-2 col-xl-2">
-                                <div class="d-flex flex-column">
-                                    <div class="v-announcement-date-title">Posted on:</div>
-                                    <div class="v-announcement-date">
-                                        {{ Carbon\Carbon::parse($announcement->created_at)->format('d F Y') }}</div>
-                                </div>
-                                @if (Auth::user()->user_role === 3 || Auth::user()->user_role === 1)
-                                    @if (Auth::user()->user_role === 1)
-                                        <div class="d-flex flex-row">
-                                            <div class="v-announcement-date-title mr-2">
-                                                <a href="/admin/announcements/{{ $announcement->id }}/edit">
-                                                    <button class="btn btn-success">Edit</button>
-                                                </a>
-                                            </div>
-                                            <div class="v-announcement-date">
-                                                <form action="/admin/announcements/{{ $announcement->id }}" method="POST">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button class="btn btn-danger">Delete</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    @elseif(Auth::user()->user_role === 3)
-                                        <div class="d-flex flex-row">
-                                            <div class="v-announcement-date-title mr-2">
-                                                <a href="/brgy_official/announcements/{{ $announcement->id }}/edit">
-                                                    <button class="btn btn-success">Edit</button>
-                                                </a>
-                                            </div>
-                                            <div class="v-announcement-date">
-                                                <form action="/brgy_official/announcements/{{ $announcement->id }}"
-                                                    method="POST">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button class="btn btn-danger">Delete</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    @endif
-                                @endif
-                            </div>
-                        </div>
-                    </li>
-
-
-                @endforeach
-
-                <!-- END OF ANNOUNCEMENT LIST -->
-            </ul>
-        </div>
         @endempty
 
     </div>
