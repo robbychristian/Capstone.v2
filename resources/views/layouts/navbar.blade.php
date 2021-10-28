813951ee2680846e12dd5566cc3b159723d79dd2
robbychristian <robbychristiandeleon@gmail.com>
2021-10-28 16:43:22 +0800

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -183,7 +183,7 @@
 
                     <li class="nav-item">
                         <a class="nav-link collapsed text-truncate submenu" href="#submenu2" data-toggle="collapse"
-                            data-target="#submenu2"><i class="fas fa-house-user mr-2"></i> <span
+                            data-target="#submenu2"><i class="fas fa-user-tag mr-2"></i> <span
                                 class="d-none d-sm-inline">User Role</span></a>
                         <div class="collapse" id="submenu2" aria-expanded="false">
                             <ul class="flex-column pl-2 nav">
