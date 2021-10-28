aed09c834e9bcade6201f35437726fdbe77dedeb
robbychristian <robbychristiandeleon@gmail.com>
2021-10-29 02:56:43 +0800

    Update ReportsController.php

--- app/Http/Controllers/Auth/ReportsController.php
+++ app/Http/Controllers/Auth/ReportsController.php
@@ -153,7 +153,7 @@ class ReportsController extends Controller
     //$report->loc_img = $request->loc_img;
     //$result = $report->save();
     return response()->json([
-        'response' => $request,
+        'userID' => $request->user_id,
         'message' => 'Success'
     ], 200);
 }
