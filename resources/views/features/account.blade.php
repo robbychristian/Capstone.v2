abefd7ae8019dd7490fd50ec67061581284337c3
robbychristian <robbychristiandeleon@gmail.com>
2021-10-28 19:16:54 +0800

    Update account.blade.php

--- resources/views/features/account.blade.php
+++ resources/views/features/account.blade.php
@@ -229,7 +229,6 @@
                         </div>
                         <div class=" d-grid gap-2 d-md-block">
                             <button class="btn btn-primary" type="submit">Submit</button>
-                            <a class="btn btn-outline-secondary" href="#" role="button">Cancel</a>
                         </div>
                     </form>
                 </div>
