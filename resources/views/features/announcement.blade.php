c85c6f5cb566e47bb53922e0a9ff17845e46c484
robbychristian <robbychristiandeleon@gmail.com>
2021-10-28 12:05:54 +0800

    Update announcement.blade.php

--- resources/views/features/announcement.blade.php
+++ resources/views/features/announcement.blade.php
@@ -108,7 +108,7 @@
             </div>
         @else
             <div class="card mt-3">
-                <div class="card-body" style="font-weight:700; font-size: 2rem;">
+                <div class="card-body">
                     There are no announcements to show.
                 </div>
             </div>
