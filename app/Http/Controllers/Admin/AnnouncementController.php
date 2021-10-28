b4012276008a206b02c88f10149213ef596e1483
robbychristian <robbychristiandeleon@gmail.com>
2021-10-28 17:13:52 +0800

    Update AnnouncementController.php

--- app/Http/Controllers/Admin/AnnouncementController.php
+++ app/Http/Controllers/Admin/AnnouncementController.php
@@ -16,7 +16,7 @@ class AnnouncementController extends Controller
      */
     public function index()
     {
-        $announcements = Announcement::all()->paginate(2);
+        $announcements = Announcement::paginate(2);
         return view('features.announcement', [
             'announcements' => $announcements
         ]);
