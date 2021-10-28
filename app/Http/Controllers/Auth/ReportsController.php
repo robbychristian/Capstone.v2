f302dcda31b86cfee6dcd96657d4a190c04a3db4
robbychristian <robbychristiandeleon@gmail.com>
2021-10-29 02:33:53 +0800

    Update ReportsController.php

--- app/Http/Controllers/Auth/ReportsController.php
+++ app/Http/Controllers/Auth/ReportsController.php
@@ -141,7 +141,7 @@ class ReportsController extends Controller
 
     public function submitReport(Request $request)
 {
-    $request->file('image')->storeAs('report_imgs', $request->user_id . '/' . $request->loc_img, '');
+    //$request->file('image')->storeAs('report_imgs', $request->user_id . '/' . $request->loc_img, '');
     $report = new Reports;
     $report->user_id = $request->user_id;
     $report->full_name = $request->full_name;
@@ -152,11 +152,6 @@ class ReportsController extends Controller
     $report->loc_lng = $request->loc_lng;
     $report->loc_img = $request->loc_img;
     $result = $report->save();
-    if ($result){
-        return ["Result" => "Saved"];
-    } else {
-        return ["Result" => "Failed"];
-    }
     return "SUCCESS";
 }
 }
