4c11d5b766f5add41294d7df5b35ceee106e5831
robbychristian <robbychristiandeleon@gmail.com>
2021-10-30 07:56:03 +0800

    Update reports.blade.php

--- resources/views/features/reports.blade.php
+++ resources/views/features/reports.blade.php
@@ -192,7 +192,7 @@
 
                 map = new google.maps.Map(document.getElementById('map'), options);
                 marker.setMap(map);
-                google.maps.event.trigger(map, 'resize');
+                //google.maps.event.trigger(map, 'resize');
             }
 
             
