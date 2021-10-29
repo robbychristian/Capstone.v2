66b56ff8a4339c9251512a29b6d1e25791c8d454
robbychristian <robbychristiandeleon@gmail.com>
2021-10-30 05:01:33 +0800

    Update reports.blade.php

--- resources/views/features/reports.blade.php
+++ resources/views/features/reports.blade.php
@@ -160,23 +160,6 @@
                 }
 
                 var map = new google.maps.Map(document.getElementById('map'), options);
-
-                //var markers = [
-                //    @foreach ($coordinates as $coordinate)
-                //        ["{{ $coordinate->lat }}","{{ $coordinate->lng }}"],
-                //    
-                //    @endforeach
-                //];
-//
-                //for (var i = 0; i < markers.length; i++) {
-                //    var location = new google.maps.LatLng(markers[i][0], markers[i][1]);
-                //    var marker = new google.maps.Marker({
-                //        position: location,
-                //        map: map
-                //    });
-//
-//
-                //}
             }
         </script>
 
