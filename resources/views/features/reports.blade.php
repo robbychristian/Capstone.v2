138b93150e1f29e3096ae6bd105a95f0b68c9c5d
robbychristian <robbychristiandeleon@gmail.com>
2021-10-30 07:18:22 +0800

    Update reports.blade.php

--- resources/views/features/reports.blade.php
+++ resources/views/features/reports.blade.php
@@ -170,19 +170,19 @@
                     zoom: 16,
                     gestureHandling: "none",
                     center: {
-                        lat: lat,
-                        lng: lng
-                    },
+                    lat: 14.6131,
+                    lng: 121.0880
+                },
                 }
 
-                var latlng = new google.maps.LatLng(lat, lng)
-
-                var marker = new google.maps.Marker({
-                    position: latlng
-                });
+                //var latlng = new google.maps.LatLng(lat, lng)
+//
+                //var marker = new google.maps.Marker({
+                //    position: latlng
+                //});
 
                 map = new google.maps.Map(document.getElementById('map'), options);
-                marker.setMap(map);
+                //marker.setMap(map);
                 google.maps.event.trigger(map, 'resize');
             }
 
