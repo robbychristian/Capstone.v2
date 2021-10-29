04b633f6737d7f0a9bf6359f7d9918ff4308c47c
robbychristian <robbychristiandeleon@gmail.com>
2021-10-30 04:09:44 +0800

    Update reports.blade.php

--- resources/views/features/reports.blade.php
+++ resources/views/features/reports.blade.php
@@ -28,7 +28,13 @@
                         @if ($report->user_id === Auth::user()->id && Auth::user()->user_role === 4)
                             <tr>
                                 <th scope="row">{{ $report->title }}</th>
-                                <td><img src="{{ URL::asset("KabisigGit/storage/app/public/report_imgs/" . $report->user_id . '/'. $report->loc_img) }}" ></td>
+                                <td>
+                                    <!--<img src="{{ URL::asset('KabisigGit/storage/app/public/report_imgs/' . $report->user_id . '/' . $report->loc_img) }}" > -->
+                                    <button type="button" class="btn btn-primary" data-toggle="modal"
+                                        data-target="#exampleModal">
+                                        Launch demo modal
+                                    </button>
+                                </td>
                                 <td>{{ $report->description }}</td>
                                 <td>{{ $report->status }}</td>
                                 <td>{{ $report->loc_lng . ' ' . $report->loc_lat }}</td>
@@ -94,9 +100,37 @@
                                 </td>
                             </tr>
                         @endif
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <button type="button" class="close" data-dismiss="modal"
+                                            aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        ...
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     @endforeach
                 </tbody>
             </table>
         </div>
+
+        <!-- Button trigger modal -->
+
+
+
     </div>
 @endsection
