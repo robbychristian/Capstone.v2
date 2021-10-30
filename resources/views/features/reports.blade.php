fd8d97fc1fcbe5ca91c355a4e82ed5a941e4bb8e
robbychristian <robbychristiandeleon@gmail.com>
2021-10-30 09:13:53 +0800

    Update reports.blade.php

--- resources/views/features/reports.blade.php
+++ resources/views/features/reports.blade.php
@@ -38,7 +38,7 @@
                                 <td>{{ $report->status }}</td>
                                 <td> <button type="button" class="btn btn-primary" data-toggle="modal"
                                         data-target="#modalMap"
-                                        onclick="initMap( 14.93921423, 123.12314123)">
+                                        onclick="initMap({{ $report->loc_lng }}, {{ $report->loc_lat }})">
                                         View Map
                                     </button>
                                     {{ $report->loc_lng . ' ' . $report->loc_lat }}</td>
@@ -155,16 +155,16 @@
             var map;
 
 
-            function initMap(loc_lat, loc_lng) {
+            function initMap(loc_lng, loc_lat) {
 
-                //let lat = loc_lat.toString()
-                //let lng = loc_lng.toString()
-                ////
-                //let floatLat = parseFloat(lat)
-                //let floatLng = parseFloat(lng)
-                //console.log(typeof(lat) + " " + lat + typeof(floatLat) + " " + floatLat)
-                //console.log(typeof(lng) + " " + lng + typeof(floatLng) + " " + floatLng)
-//
+                let lat = loc_lat.toString()
+                let lng = loc_lng.toString()
+                //
+                let floatLat = parseFloat(lat)
+                let floatLng = parseFloat(lng)
+                console.log(typeof(lat) + " " + lat + typeof(floatLat) + " " + floatLat)
+                console.log(typeof(lng) + " " + lng + typeof(floatLng) + " " + floatLng)
+
                 //var location = {
                 //    lat: Number(loc_lat),
                 //    lng: Number(loc_lng)
@@ -176,12 +176,12 @@
                     //    lat: floatLat,
                     //    lng: floatLng
                     //},
-                    center: new google.maps.LatLng(loc_lat, loc_lng)
+                    center: new google.maps.LatLng(floatLng, floatLat)
                 }
 
                 console.log(options.center)
 
-                var latlng = new google.maps.LatLng(loc_lat, loc_lng)
+                var latlng = new google.maps.LatLng(floatLng, floatLat)
 
                 var marker = new google.maps.Marker({
                     position: latlng
