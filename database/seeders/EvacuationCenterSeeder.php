76db1ecb36f328ef393587efb4f4bdba2226a3cd
robbychristian <robbychristiandeleon@gmail.com>
2021-11-01 01:50:13 +0800

    Update EvacuationCenterSeeder.php

--- database/seeders/EvacuationCenterSeeder.php
+++ database/seeders/EvacuationCenterSeeder.php
@@ -15,21 +15,25 @@ class EvacuationCenterSeeder extends Seeder
     public function run()
     {
         DB::table('evacuation_centers')->insert([
+            'brgy_loc' => 'Barangay Santolan',
             'lat' => '14.612163189054954',
             'lng' => '121.09191240222641'
         ]);
 
         DB::table('evacuation_centers')->insert([
+            'brgy_loc' => 'Barangay Santolan',
             'lat' => '14.61561537382943',
             'lng' => '121.08202130848416'
         ]);
 
         DB::table('evacuation_centers')->insert([
+            'brgy_loc' => 'Barangay Santolan',
             'lat' => '14.616839189248806',
             'lng' => '121.08160482875117'
         ]);
 
         DB::table('evacuation_centers')->insert([
+            'brgy_loc' => 'Barangay Santolan',
             'lat' => '14.616544695649692',
             'lng' => '121.08636442213766'
         ]);
