d835e397916d38e7520a4d1a434a5d0656a7c602
robbychristian <robbychristiandeleon@gmail.com>
2021-11-01 15:16:53 +0800

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -180,7 +180,7 @@ class RegisterController extends Controller
         event(new Registered($user = $this->create($request->all())));
 
         // Disabling the auto login on register
-        $this->guard()->login($user);
+        //$this->guard()->login($user);
 
         // return $this->registered($request, $user)
         //     ?: redirect($this->redirectPath());
