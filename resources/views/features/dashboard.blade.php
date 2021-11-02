21782c8612bf6f0576245ed53e74143f56842e5f
robbychristian <robbychristiandeleon@gmail.com>
2021-11-03 02:06:56 +0800

    Update dashboard.blade.php

--- resources/views/features/dashboard.blade.php
+++ resources/views/features/dashboard.blade.php
@@ -30,8 +30,8 @@
                 <div class="col-sm-6 mb-3">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Barangay Dela Paz</h5>
-                            <p class="card-text">View the Disaster Statistics Report of Barangay Dela Paz
+                            <h5 class="card-title">Barangay Manggahan</h5>
+                            <p class="card-text">View the Disaster Statistics Report of Barangay Manggahan
                             </p>
                             <a href="#" class="btn btn-primary">View</a>
                         </div>
@@ -42,18 +42,30 @@
                 <div class="col-sm-6 mb-3">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Barangay Dela Paz</h5>
-                            <p class="card-text">View the Disaster Statistics Report of Barangay Dela Paz
+                            <h5 class="card-title">Barangay Maybunga</h5>
+                            <p class="card-text">View the Disaster Statistics Report of Barangay Maybunga
                             </p>
                             <a href="#" class="btn btn-primary">View</a>
                         </div>
                     </div>
                 </div>
+
                 <div class="col-sm-6 mb-3">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Barangay Dela Paz</h5>
-                            <p class="card-text">View the Disaster Statistics Report of Barangay Dela Paz
+                            <h5 class="card-title">Barangay Rosario</h5>
+                            <p class="card-text">View the Disaster Statistics Report of Barangay Rosario
+                            </p>
+                            <a href="#" class="btn btn-primary">View</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Barangay Santolan</h5>
+                            <p class="card-text">View the Disaster Statistics Report of Barangay Santolan
                             </p>
                             <a href="#" class="btn btn-primary">View</a>
                         </div>
