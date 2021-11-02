5c5ab536234d918029368dccf1dc07e6a2e65b85
robbychristian <robbychristiandeleon@gmail.com>
2021-11-03 06:44:55 +0800

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -90,7 +90,7 @@ class DashboardController extends Controller
     {
         // ADD WHERE CLAUSE IN BRGY
         $date = Carbon::now()->format('Y');
-        $brgy_loc = "Barangay " . $brgy;
+        $brgy_loc = "Barangay ".$brgy;
         //january
         $jan_typhoon_count = DisasterReport::where('type_disaster', 'Typhoon')
             ->where('month_disaster', 'January')
@@ -1066,290 +1066,292 @@ class DashboardController extends Controller
             ->where('year_disaster', $date)
             ->sum('evacuees');
 
-        return $brgy_loc;
-        return view('features.dashboard', compact(
-            'jan_typhoon_count',
-            'jan_flood_count',
-            'jan_lpa_count',
-            'jan_earthquake_count',
-            'jan_landslide_count',
-            'jan_others_count',
 
-
-            'feb_typhoon_count',
-            'feb_flood_count',
-            'feb_lpa_count',
-            'feb_earthquake_count',
-            'feb_landslide_count',
-            'feb_others_count',
-
-
-            'mar_typhoon_count',
-            'mar_flood_count',
-            'mar_lpa_count',
-            'mar_earthquake_count',
-            'mar_landslide_count',
-            'mar_others_count',
-
-
-            'apr_typhoon_count',
-            'apr_flood_count',
-            'apr_lpa_count',
-            'apr_earthquake_count',
-            'apr_landslide_count',
-            'apr_others_count',
-
-
-            'may_typhoon_count',
-            'may_flood_count',
-            'may_lpa_count',
-            'may_earthquake_count',
-            'may_landslide_count',
-            'may_others_count',
-
-
-            'jun_typhoon_count',
-            'jun_flood_count',
-            'jun_lpa_count',
-            'jun_earthquake_count',
-            'jun_landslide_count',
-            'jun_others_count',
-
-
-            'jul_typhoon_count',
-            'jul_flood_count',
-            'jul_lpa_count',
-            'jul_earthquake_count',
-            'jul_landslide_count',
-            'jul_others_count',
-
-
-            'aug_typhoon_count',
-            'aug_flood_count',
-            'aug_lpa_count',
-            'aug_earthquake_count',
-            'aug_landslide_count',
-            'aug_others_count',
-
-
-            'sept_typhoon_count',
-            'sept_flood_count',
-            'sept_lpa_count',
-            'sept_earthquake_count',
-            'sept_landslide_count',
-            'sept_others_count',
-
-
-            'oct_typhoon_count',
-            'oct_flood_count',
-            'oct_lpa_count',
-            'oct_earthquake_count',
-            'oct_landslide_count',
-            'oct_others_count',
-
-            'nov_typhoon_count',
-            'nov_flood_count',
-            'nov_lpa_count',
-            'nov_earthquake_count',
-            'nov_landslide_count',
-            'nov_others_count',
-
-            'dec_typhoon_count',
-            'dec_flood_count',
-            'dec_lpa_count',
-            'dec_earthquake_count',
-            'dec_landslide_count',
-            'dec_others_count',
-
-
-            'jan_typhoon_count_indiv',
-            'jan_flood_count_indiv',
-            'jan_lpa_count_indiv',
-            'jan_earthquake_count_indiv',
-            'jan_landslide_count_indiv',
-            'jan_others_count_indiv',
-
-
-            'feb_typhoon_count_indiv',
-            'feb_flood_count_indiv',
-            'feb_lpa_count_indiv',
-            'feb_earthquake_count_indiv',
-            'feb_landslide_count_indiv',
-            'feb_others_count_indiv',
-
-            'mar_typhoon_count_indiv',
-            'mar_flood_count_indiv',
-            'mar_lpa_count_indiv',
-            'mar_earthquake_count_indiv',
-            'mar_landslide_count_indiv',
-            'mar_others_count_indiv',
-
-
-            'apr_typhoon_count_indiv',
-            'apr_flood_count_indiv',
-            'apr_lpa_count_indiv',
-            'apr_earthquake_count_indiv',
-            'apr_landslide_count_indiv',
-            'apr_others_count_indiv',
-
-            'may_typhoon_count_indiv',
-            'may_flood_count_indiv',
-            'may_lpa_count_indiv',
-            'may_earthquake_count_indiv',
-            'may_landslide_count_indiv',
-            'may_others_count_indiv',
-
-
-            'jun_typhoon_count_indiv',
-            'jun_flood_count_indiv',
-            'jun_lpa_count_indiv',
-            'jun_earthquake_count_indiv',
-            'jun_landslide_count_indiv',
-            'jun_others_count_indiv',
-
-            'jul_typhoon_count_indiv',
-            'jul_flood_count_indiv',
-            'jul_lpa_count_indiv',
-            'jul_earthquake_count_indiv',
-            'jul_landslide_count_indiv',
-            'jul_others_count_indiv',
-
-
-            'aug_typhoon_count_indiv',
-            'aug_flood_count_indiv',
-            'aug_lpa_count_indiv',
-            'aug_earthquake_count_indiv',
-            'aug_landslide_count_indiv',
-            'aug_others_count_indiv',
-
-
-            'sept_typhoon_count_indiv',
-            'sept_flood_count_indiv',
-            'sept_lpa_count_indiv',
-            'sept_earthquake_count_indiv',
-            'sept_landslide_count_indiv',
-            'sept_others_count_indiv',
-
-
-            'oct_typhoon_count_indiv',
-            'oct_flood_count_indiv',
-            'oct_lpa_count_indiv',
-            'oct_earthquake_count_indiv',
-            'oct_landslide_count_indiv',
-            'oct_others_count_indiv',
-
-
-            'nov_typhoon_count_indiv',
-            'nov_flood_count_indiv',
-            'nov_lpa_count_indiv',
-            'nov_earthquake_count_indiv',
-            'nov_landslide_count_indiv',
-            'nov_others_count_indiv',
-
-
-            'dec_typhoon_count_indiv',
-            'dec_flood_count_indiv',
-            'dec_lpa_count_indiv',
-            'dec_earthquake_count_indiv',
-            'dec_landslide_count_indiv',
-            'dec_others_count_indiv',
-
-
-            'jan_typhoon_count_evac',
-            'jan_flood_count_evac',
-            'jan_lpa_count_evac',
-            'jan_earthquake_count_evac',
-            'jan_landslide_count_evac',
-            'jan_others_count_evac',
-
-
-            'feb_typhoon_count_evac',
-            'feb_flood_count_evac',
-            'feb_lpa_count_evac',
-            'feb_earthquake_count_evac',
-            'feb_landslide_count_evac',
-            'feb_others_count_evac',
-
-
-            'mar_typhoon_count_evac',
-            'mar_flood_count_evac',
-            'mar_lpa_count_evac',
-            'mar_earthquake_count_evac',
-            'mar_landslide_count_evac',
-            'mar_others_count_evac',
-
-
-            'apr_typhoon_count_evac',
-            'apr_flood_count_evac',
-            'apr_lpa_count_evac',
-            'apr_earthquake_count_evac',
-            'apr_landslide_count_evac',
-            'apr_others_count_evac',
-
-
-            'may_typhoon_count_evac',
-            'may_flood_count_evac',
-            'may_lpa_count_evac',
-            'may_earthquake_count_evac',
-            'may_landslide_count_evac',
-            'may_others_count_evac',
-
-
-            'jun_typhoon_count_evac',
-            'jun_flood_count_evac',
-            'jun_lpa_count_evac',
-            'jun_earthquake_count_evac',
-            'jun_landslide_count_evac',
-            'jun_others_count_evac',
-
-
-            'jul_typhoon_count_evac',
-            'jul_flood_count_evac',
-            'jul_lpa_count_evac',
-            'jul_earthquake_count_evac',
-            'jul_landslide_count_evac',
-            'jul_others_count_evac',
-
-            'aug_typhoon_count_evac',
-            'aug_flood_count_evac',
-            'aug_lpa_count_evac',
-            'aug_earthquake_count_evac',
-            'aug_landslide_count_evac',
-            'aug_others_count_evac',
-
-
-            'sept_typhoon_count_evac',
-            'sept_flood_count_evac',
-            'sept_lpa_count_evac',
-            'sept_earthquake_count_evac',
-            'sept_landslide_count_evac',
-            'sept_others_count_evac',
-
-
-            'oct_typhoon_count_evac',
-            'oct_flood_count_evac',
-            'oct_lpa_count_evac',
-            'oct_earthquake_count_evac',
-            'oct_landslide_count_evac',
-            'oct_others_count_evac',
-
-
-            'nov_typhoon_count_evac',
-            'nov_flood_count_evac',
-            'nov_lpa_count_evac',
-            'nov_earthquake_count_evac',
-            'nov_landslide_count_evac',
-            'nov_others_count_evac',
-
-
-            'dec_typhoon_count_evac',
-            'dec_flood_count_evac',
-            'dec_lpa_count_evac',
-            'dec_earthquake_count_evac',
-            'dec_landslide_count_evac',
-            'dec_others_count_evac',
-
-
-        ));
+        //return view('features.dashboard', compact(
+        //    'jan_typhoon_count',
+        //    'jan_flood_count',
+        //    'jan_lpa_count',
+        //    'jan_earthquake_count',
+        //    'jan_landslide_count',
+        //    'jan_others_count',
+//
+//
+        //    'feb_typhoon_count',
+        //    'feb_flood_count',
+        //    'feb_lpa_count',
+        //    'feb_earthquake_count',
+        //    'feb_landslide_count',
+        //    'feb_others_count',
+//
+//
+        //    'mar_typhoon_count',
+        //    'mar_flood_count',
+        //    'mar_lpa_count',
+        //    'mar_earthquake_count',
+        //    'mar_landslide_count',
+        //    'mar_others_count',
+//
+//
+        //    'apr_typhoon_count',
+        //    'apr_flood_count',
+        //    'apr_lpa_count',
+        //    'apr_earthquake_count',
+        //    'apr_landslide_count',
+        //    'apr_others_count',
+//
+//
+        //    'may_typhoon_count',
+        //    'may_flood_count',
+        //    'may_lpa_count',
+        //    'may_earthquake_count',
+        //    'may_landslide_count',
+        //    'may_others_count',
+//
+//
+        //    'jun_typhoon_count',
+        //    'jun_flood_count',
+        //    'jun_lpa_count',
+        //    'jun_earthquake_count',
+        //    'jun_landslide_count',
+        //    'jun_others_count',
+//
+//
+        //    'jul_typhoon_count',
+        //    'jul_flood_count',
+        //    'jul_lpa_count',
+        //    'jul_earthquake_count',
+        //    'jul_landslide_count',
+        //    'jul_others_count',
+//
+//
+        //    'aug_typhoon_count',
+        //    'aug_flood_count',
+        //    'aug_lpa_count',
+        //    'aug_earthquake_count',
+        //    'aug_landslide_count',
+        //    'aug_others_count',
+//
+//
+        //    'sept_typhoon_count',
+        //    'sept_flood_count',
+        //    'sept_lpa_count',
+        //    'sept_earthquake_count',
+        //    'sept_landslide_count',
+        //    'sept_others_count',
+//
+//
+        //    'oct_typhoon_count',
+        //    'oct_flood_count',
+        //    'oct_lpa_count',
+        //    'oct_earthquake_count',
+        //    'oct_landslide_count',
+        //    'oct_others_count',
+//
+        //    'nov_typhoon_count',
+        //    'nov_flood_count',
+        //    'nov_lpa_count',
+        //    'nov_earthquake_count',
+        //    'nov_landslide_count',
+        //    'nov_others_count',
+//
+        //    'dec_typhoon_count',
+        //    'dec_flood_count',
+        //    'dec_lpa_count',
+        //    'dec_earthquake_count',
+        //    'dec_landslide_count',
+        //    'dec_others_count',
+//
+//
+        //    'jan_typhoon_count_indiv',
+        //    'jan_flood_count_indiv',
+        //    'jan_lpa_count_indiv',
+        //    'jan_earthquake_count_indiv',
+        //    'jan_landslide_count_indiv',
+        //    'jan_others_count_indiv',
+//
+//
+        //    'feb_typhoon_count_indiv',
+        //    'feb_flood_count_indiv',
+        //    'feb_lpa_count_indiv',
+        //    'feb_earthquake_count_indiv',
+        //    'feb_landslide_count_indiv',
+        //    'feb_others_count_indiv',
+//
+        //    'mar_typhoon_count_indiv',
+        //    'mar_flood_count_indiv',
+        //    'mar_lpa_count_indiv',
+        //    'mar_earthquake_count_indiv',
+        //    'mar_landslide_count_indiv',
+        //    'mar_others_count_indiv',
+//
+//
+        //    'apr_typhoon_count_indiv',
+        //    'apr_flood_count_indiv',
+        //    'apr_lpa_count_indiv',
+        //    'apr_earthquake_count_indiv',
+        //    'apr_landslide_count_indiv',
+        //    'apr_others_count_indiv',
+//
+        //    'may_typhoon_count_indiv',
+        //    'may_flood_count_indiv',
+        //    'may_lpa_count_indiv',
+        //    'may_earthquake_count_indiv',
+        //    'may_landslide_count_indiv',
+        //    'may_others_count_indiv',
+//
+//
+        //    'jun_typhoon_count_indiv',
+        //    'jun_flood_count_indiv',
+        //    'jun_lpa_count_indiv',
+        //    'jun_earthquake_count_indiv',
+        //    'jun_landslide_count_indiv',
+        //    'jun_others_count_indiv',
+//
+        //    'jul_typhoon_count_indiv',
+        //    'jul_flood_count_indiv',
+        //    'jul_lpa_count_indiv',
+        //    'jul_earthquake_count_indiv',
+        //    'jul_landslide_count_indiv',
+        //    'jul_others_count_indiv',
+//
+//
+        //    'aug_typhoon_count_indiv',
+        //    'aug_flood_count_indiv',
+        //    'aug_lpa_count_indiv',
+        //    'aug_earthquake_count_indiv',
+        //    'aug_landslide_count_indiv',
+        //    'aug_others_count_indiv',
+//
+//
+        //    'sept_typhoon_count_indiv',
+        //    'sept_flood_count_indiv',
+        //    'sept_lpa_count_indiv',
+        //    'sept_earthquake_count_indiv',
+        //    'sept_landslide_count_indiv',
+        //    'sept_others_count_indiv',
+//
+//
+        //    'oct_typhoon_count_indiv',
+        //    'oct_flood_count_indiv',
+        //    'oct_lpa_count_indiv',
+        //    'oct_earthquake_count_indiv',
+        //    'oct_landslide_count_indiv',
+        //    'oct_others_count_indiv',
+//
+//
+        //    'nov_typhoon_count_indiv',
+        //    'nov_flood_count_indiv',
+        //    'nov_lpa_count_indiv',
+        //    'nov_earthquake_count_indiv',
+        //    'nov_landslide_count_indiv',
+        //    'nov_others_count_indiv',
+//
+//
+        //    'dec_typhoon_count_indiv',
+        //    'dec_flood_count_indiv',
+        //    'dec_lpa_count_indiv',
+        //    'dec_earthquake_count_indiv',
+        //    'dec_landslide_count_indiv',
+        //    'dec_others_count_indiv',
+//
+//
+        //    'jan_typhoon_count_evac',
+        //    'jan_flood_count_evac',
+        //    'jan_lpa_count_evac',
+        //    'jan_earthquake_count_evac',
+        //    'jan_landslide_count_evac',
+        //    'jan_others_count_evac',
+//
+//
+        //    'feb_typhoon_count_evac',
+        //    'feb_flood_count_evac',
+        //    'feb_lpa_count_evac',
+        //    'feb_earthquake_count_evac',
+        //    'feb_landslide_count_evac',
+        //    'feb_others_count_evac',
+//
+//
+        //    'mar_typhoon_count_evac',
+        //    'mar_flood_count_evac',
+        //    'mar_lpa_count_evac',
+        //    'mar_earthquake_count_evac',
+        //    'mar_landslide_count_evac',
+        //    'mar_others_count_evac',
+//
+//
+        //    'apr_typhoon_count_evac',
+        //    'apr_flood_count_evac',
+        //    'apr_lpa_count_evac',
+        //    'apr_earthquake_count_evac',
+        //    'apr_landslide_count_evac',
+        //    'apr_others_count_evac',
+//
+//
+        //    'may_typhoon_count_evac',
+        //    'may_flood_count_evac',
+        //    'may_lpa_count_evac',
+        //    'may_earthquake_count_evac',
+        //    'may_landslide_count_evac',
+        //    'may_others_count_evac',
+//
+//
+        //    'jun_typhoon_count_evac',
+        //    'jun_flood_count_evac',
+        //    'jun_lpa_count_evac',
+        //    'jun_earthquake_count_evac',
+        //    'jun_landslide_count_evac',
+        //    'jun_others_count_evac',
+//
+//
+        //    'jul_typhoon_count_evac',
+        //    'jul_flood_count_evac',
+        //    'jul_lpa_count_evac',
+        //    'jul_earthquake_count_evac',
+        //    'jul_landslide_count_evac',
+        //    'jul_others_count_evac',
+//
+        //    'aug_typhoon_count_evac',
+        //    'aug_flood_count_evac',
+        //    'aug_lpa_count_evac',
+        //    'aug_earthquake_count_evac',
+        //    'aug_landslide_count_evac',
+        //    'aug_others_count_evac',
+//
+//
+        //    'sept_typhoon_count_evac',
+        //    'sept_flood_count_evac',
+        //    'sept_lpa_count_evac',
+        //    'sept_earthquake_count_evac',
+        //    'sept_landslide_count_evac',
+        //    'sept_others_count_evac',
+//
+//
+        //    'oct_typhoon_count_evac',
+        //    'oct_flood_count_evac',
+        //    'oct_lpa_count_evac',
+        //    'oct_earthquake_count_evac',
+        //    'oct_landslide_count_evac',
+        //    'oct_others_count_evac',
+//
+//
+        //    'nov_typhoon_count_evac',
+        //    'nov_flood_count_evac',
+        //    'nov_lpa_count_evac',
+        //    'nov_earthquake_count_evac',
+        //    'nov_landslide_count_evac',
+        //    'nov_others_count_evac',
+//
+//
+        //    'dec_typhoon_count_evac',
+        //    'dec_flood_count_evac',
+        //    'dec_lpa_count_evac',
+        //    'dec_earthquake_count_evac',
+        //    'dec_landslide_count_evac',
+        //    'dec_others_count_evac',
+//
+//
+        //));
+
+        return view('features.account');
     }
 }
