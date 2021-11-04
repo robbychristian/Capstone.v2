c500071e8dd35eb5494e9419e8057270a0b6b558
robbychristian <robbychristiandeleon@gmail.com>
2021-11-04 18:48:52 +0800

    Update evacuationcenter.blade.php

--- resources/views/features/evacuationcenter.blade.php
+++ resources/views/features/evacuationcenter.blade.php
@@ -5,15 +5,55 @@
 
     <script type="text/javascript">
         function initMap() {
-            var options = {
+            @if (Auth::user()->brgy_loc == 'Barangay Santolan' || Auth::user()->user_role == 1)
+                var options = {
                 zoom: 16,
-                gestureHandling: "none",
-                zoomControl: false,
                 center: {
-                    lat: 14.6131,
-                    lng: 121.0880
+                lat: 14.6131,
+                lng: 121.0880
                 },
-            }
+                }
+            @endif
+
+            @if (Auth::user()->brgy_loc == 'Barangay Dela Paz')
+                var options = {
+                zoom: 16,
+                center: {
+                lat: 14.6137,
+                lng: 121.0960
+                },
+                }
+            @endif
+
+            @if (Auth::user()->brgy_loc == 'Barangay Manggahan')
+                var options = {
+                zoom: 16,
+                center: {
+                lat: 14.601887,
+                lng: 121.093698
+                },
+                }
+            @endif
+
+            @if (Auth::user()->brgy_loc == 'Barangay Maybunga')
+                var options = {
+                zoom: 16,
+                center: {
+                lat: 14.5763,
+                lng: 121.0850
+                },
+                }
+            @endif
+
+            @if (Auth::user()->brgy_loc == 'Barangay Rosario')
+                var options = {
+                zoom: 16,
+                center: {
+                lat: 14.5885,
+                lng: 121.0891
+                },
+                }
+            @endif
 
             var map = new google.maps.Map(document.getElementById('map'), options);
 
@@ -23,6 +63,36 @@
                 
                 @endforeach
             ];
+
+            function newLocation(newLat, newLng) {
+                map.setCenter({
+                    lat: newLat,
+                    lng: newLng
+                });
+            }
+            //Setting Location with jQuery
+            $(document).ready(function() {
+                $("#brgyDelaPaz").on('click', function() {
+                    newLocation(14.6137, 121.0960);
+                });
+
+                $("#brgyManggahan").on('click', function() {
+                    newLocation(14.601887, 121.093698);
+                });
+
+                $("#brgyMaybunga").on('click', function() {
+                    newLocation(14.5763, 121.0850);
+                });
+
+                $("#brgySantolan").on('click', function() {
+                    newLocation(14.6131, 121.0880);
+                });
+
+                $("#brgyRosario").on('click', function() {
+                    newLocation(14.5885, 121.0891);
+                });
+            });
+
 
             for (var i = 0; i < markers.length; i++) {
                 var location = new google.maps.LatLng(markers[i][0], markers[i][1]);
@@ -37,7 +107,32 @@
     </script>
     <div class="col-xl-10 col-lg-9 col-md-8 mt-3">
         <h3 class="mb-3">Evacuation Centers and Nearby Hospitals</h3>
-        <div id="map" style="height: 600px; width: 100%;"></div>
+        @if (Auth::user()->user_role === 1)
+            <form class="mb-3">
+                <p class="vul-caption mt-3">Choose the barangay:</p>
+                <div class="radio-button-wrap mt-2">
+                    <input type="radio" name="brgy_loc" id="brgyDelaPaz" value="Barangay Dela Paz" hidden>
+                    <label for="brgyDelaPaz" class="button-label">Barangay Dela Paz</label>
+
+                    <input type="radio" name="brgy_loc" id="brgyManggahan" value="Barangay Manggahan" hidden>
+                    <label for="brgyManggahan" class="button-label">Barangay Manggahan</label>
+
+                    <input type="radio" name="brgy_loc" id="brgyMaybunga" value="Barangay Maybunga" hidden>
+                    <label for="brgyMaybunga" class="button-label">Barangay Maybunga</label>
+
+                    <input type="radio" name="brgy_loc" id="brgySantolan" value="Barangay Santolan" hidden>
+                    <label for="brgySantolan" class="button-label">Barangay Santolan</label>
+
+                    <input type="radio" name="brgy_loc" id="brgyRosario" value="Barangay Rosario" hidden>
+                    <label for="brgyRosario" class="button-label">Barangay Rosario</label>
+                </div>
+            </form>
+        @endif
+        <div class="card shadow-card mb-3 mt-3">
+            <div class="card-body">
+                <div id="map" style="height: 600px; width: 100%;"></div>
+            </div>
+        </div>
     </div>
 
     <script async defer
