97e69b9003100da170a114921912b566360c09d4
robbychristian <robbychristiandeleon@gmail.com>
2021-11-04 12:04:01 +0800

    Update editannouncement.blade.php

--- resources/views/features/editannouncement.blade.php
+++ resources/views/features/editannouncement.blade.php
@@ -14,22 +14,6 @@
             <label><strong>Title</strong></label>
             <input type="text" name="title" class="form-control" value="{{ $announcement->title }}">
         </div>
-        @if (Auth::user()->user_role === 1)
-            <div class="form-group">
-                <label><strong>Recipients</strong></label>
-                <select name="brgy_loc" class="form-control" value="{{ old('brgy_loc') }}">
-                    <option value="">Barangay</option>
-                    <option value='Barangay Dela Paz'>Barangay Dela Paz</option>
-                    <option value='Barangay Manggahan'>Barangay Manggahan</option>
-                    <option value='Barangay Maybunga'>Barangay Maybunga</option>
-                    <option value='Barangay Rosario'>Barangay Rosario</option>
-                    <option value='Barangay Santolan'>Barangay Santolan</option>
-                </select>
-                <small class="text-danger">@error('brgy_loc')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-        @endif
         <div class="form-group">
             <label><strong>Message</strong></label>
             <textarea name="message" id="" cols="30" rows="10" class="form-control">{{ $announcement->body }}</textarea>
