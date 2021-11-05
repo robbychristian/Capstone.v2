949b601536ee2f8d7430a572bd822077a7af1046
robbychristian <robbychristiandeleon@gmail.com>
2021-11-05 15:42:14 +0800

    Update EmergencyController.php

--- app/Http/Controllers/Brgy/EmergencyController.php
+++ app/Http/Controllers/Brgy/EmergencyController.php
@@ -29,6 +29,21 @@ class EmergencyController extends Controller
         return view('features.emergencymessage');
     }
 
+    function itexmo($number, $message, $apicode, $passwd)
+    {
+        $url = 'https://www.itexmo.com/php_api/api.php';
+        $itexmo = array('1' => $number, '2' => $message, '3' => $apicode, 'passwd' => $passwd);
+        $param = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($itexmo),
+            ),
+        );
+        $context  = stream_context_create($param);
+        return file_get_contents($url, false, $context);
+    }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -44,14 +59,27 @@ class EmergencyController extends Controller
             'recipients.required' => 'The recipient field is required!',
             'message.required' => 'The body field is required!',
         ]);
+        
+        $number = $request->input('recipients');
+        $message = $request->input('message');
+        $apicode = "TR-CHRIS079696_7PYK4";
+        $apipwd = "pdawzaamb7";
+
 
         if ($validator->fails()) {
             return redirect('/brgy_official/emergencymessage/create')
                 ->withErrors($validator)
                 ->withInput();
         } else {
-
-            return redirect('/brgy_official/emergencymessage')->with('success', 'Emergency Alert Message has been sent!');
+            $result = $this->itexmo($number, $message, $apicode, $apipwd);
+            if($result == ""){
+                echo "iTexMo: No response from server!!! Please check the METHOD used (CURL or CURL-LESS). If you are using CURL then try CURL-LESS and vice versa.	Please CONTACT US for help. ";	
+            }else if ($result == 0){
+                echo "Message sent";
+            }else{
+                echo "Error Number " . $result . "was encountered!";
+            }
+            //return redirect('/brgy_official/emergencymessage')->with('success', 'Emergency Alert Message has been sent!');
         }
     }
 
