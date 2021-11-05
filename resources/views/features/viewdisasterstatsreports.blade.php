94d29c96ff333800e882c881ffb4b909f4d2e8be
robbychristian <robbychristiandeleon@gmail.com>
2021-11-06 00:39:36 +0800

    Update viewdisasterstatsreports.blade.php

--- resources/views/features/viewdisasterstatsreports.blade.php
+++ resources/views/features/viewdisasterstatsreports.blade.php
@@ -19,26 +19,26 @@
         </div>
 
         @if (Session::get('success'))
-            <div class="alert alert-success">
+            <div class="alert alert-success mt-3">
                 {{ Session::get('success') }}
             </div>
 
         @endif
 
         @if (count($disasterstats) > 0)
-            <div class="table-responsive mt-3">
+            <div class="table-responsive mt-5">
                 <table class="table table-hover">
-                    <thead>
+                    <thead style="background-color: #004f91;">
                         <tr>
-                            <th scope="col">Created At</th>
-                            <th scope="col">Month of Disaster</th>
-                            <th scope="col">Year of Disaster</th>
-                            <th scope="col">Type of Disaster</th>
-                            <th scope="col">Barangay</th>
-                            <th scope="col">Families Affected</th>
-                            <th scope="col">Individuals Affected</th>
-                            <th scope="col">Evacuees</th>
-                            <th scope="col" colspan="2">Action</th>
+                            <th scope="col" style="color: white;">Created At</th>
+                            <th scope="col" style="color: white;">Month of Disaster</th>
+                            <th scope="col" style="color: white;">Year of Disaster</th>
+                            <th scope="col" style="color: white;">Type of Disaster</th>
+                            <th scope="col" style="color: white;">Barangay</th>
+                            <th scope="col" style="color: white;">Families Affected</th>
+                            <th scope="col" style="color: white;">Individuals Affected</th>
+                            <th scope="col" style="color: white;">Evacuees</th>
+                            <th scope="col" style="color: white;" colspan="2">Action</th>
                         </tr>
                     </thead>
                     <tbody>
