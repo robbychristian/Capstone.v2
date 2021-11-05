3edfd94994878ea21162df6bc2f802f5f9d88628
robbychristian <robbychristiandeleon@gmail.com>
2021-11-05 16:35:55 +0800

    Update EmergencyController.php

--- app/Http/Controllers/Brgy/EmergencyController.php
+++ app/Http/Controllers/Brgy/EmergencyController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Brgy;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserProfile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 
@@ -65,22 +68,28 @@ class EmergencyController extends Controller
         $apicode = "TR-CHRIS079696_7PYK4";
         $apipwd = "pdawzaamb7";
 
+        $brgyloc = Auth::user()->brgy_loc;
+        $number = DB::table('user_profiles')
+                    ->where('contact_no', '=', $brgyloc)
+                    ->get();
 
-        if ($validator->fails()) {
-            return redirect('/brgy_official/emergencymessage/create')
-                ->withErrors($validator)
-                ->withInput();
-        } else {
-            $result = $this->itexmo($number, $message, $apicode, $apipwd);
-            if($result == ""){
-                return redirect('/brgy_official/emergencymessage/create')->with('success', 'Something went wrong!');	
-            }else if ($result == 0){
-                return redirect('/brgy_official/emergencymessage/create')->with('success', 'Message sent!');
-            }else{
-                return redirect('/brgy_official/emergencymessage/create')->with('success', 'Error was encountered!');
-            }
-            
-        }
+        dd($number);
+
+       //if ($validator->fails()) {
+       //    return redirect('/brgy_official/emergencymessage/create')
+       //        ->withErrors($validator)
+       //        ->withInput();
+       //} else {
+       //    $result = $this->itexmo($number, $message, $apicode, $apipwd);
+       //    if($result == ""){
+       //        return redirect('/brgy_official/emergencymessage/create')->with('success', 'Something went wrong!');	
+       //    }else if ($result == 0){
+       //        return redirect('/brgy_official/emergencymessage/create')->with('success', 'Message sent!');
+       //    }else{
+       //        return redirect('/brgy_official/emergencymessage/create')->with('success', 'Error was encountered!');
+       //    }
+       //    
+       //}
     }
 
     /**
