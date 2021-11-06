aee2d9842bf73fdc2b21acef3cbc9965febbca0b
robbychristian <robbychristiandeleon@gmail.com>
2021-11-07 07:45:10 +0800

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,11 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
-            $test = (new MailMessage)
+            return (new MailMessage)
                 ->subject('Verify Email Address')
                 ->line('Click the button below to verify your email address.')
                 ->action('Verify Email Address', $url);
-            return redirect('user/login');
         });
     }
 }
