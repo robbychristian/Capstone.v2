a474f15aafdc72191ace784e8eb9284d37aa6a5f
robbychristian <robbychristiandeleon@gmail.com>
2021-11-14 23:53:08 +0800

    Update announcement.blade.php

--- resources/views/features/announcement.blade.php
+++ resources/views/features/announcement.blade.php
@@ -3,7 +3,7 @@
 @section('title', '| Announcements')
 @section('content')
 
-    <div class="container-fluid">
+    <div class="container-fluid" style="color: black;">
 
         <div class="row">
             <div class="col-sm-12 col-md-8">
