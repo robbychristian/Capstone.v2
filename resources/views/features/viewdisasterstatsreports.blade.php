cf5196d112357e9034bb2d3eae5f57116ade0ba6
robbychristian <robbychristiandeleon@gmail.com>
2021-11-15 03:32:44 +0800

    Update viewdisasterstatsreports.blade.php

--- resources/views/features/viewdisasterstatsreports.blade.php
+++ resources/views/features/viewdisasterstatsreports.blade.php
@@ -2,20 +2,12 @@
 
 @section('title', '| Disaster Statistical Reports')
 @section('content')
-    <div class="col-xl-10 col-lg-9 col-md-8 mt-3">
-        <div class="row">
-            <div class="col-sm-12 col-md-8">
-                <h1 class="h3 mb-4 text-gray-800">Disaster Statistical Reports</h1>
-            </div>
-
-            <!--ACCESSIBLE ONLY TO BRGY-->
-            <div class="col-sm-12 col-md-4">
-                <div class="row"></div>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                    <a href="{{ route('brgy_official.stats.create') }}" class="btn btn-primary" role="button">Create a
-                        Disaster Statistical Reports</a>
-                </div>
-            </div>
+    <div class="container-fluid" style="color: black;">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Disaster Statistical Reports</h1>
+            <a href="{{ route('brgy_official.stats.create') }}" class="d-sm-inline-block btn btn-primary shadow-sm"><i
+                    class="fas fa-download fa-sm text-white-50"></i> Create a
+                Disaster Statistical Reports</a>
         </div>
 
         @if (Session::get('success'))
