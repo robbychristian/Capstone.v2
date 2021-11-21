d85366693d9b74468c31acfa0cd039abe2597d30
robbychristian <robbychristiandeleon@gmail.com>
2021-11-21 09:52:48 +0800

    stats

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -131,7 +131,15 @@ class StatisticsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $disasterstats = DisasterReport::find($id);
+        $affectedstreets = DisasterReport::find($id)->affectedStreets;
+
+        //return $affectedstreets;
+        return view('features.editdisasterstatsreports', [
+            'disasterstats' => $disasterstats,
+            'affectedstreets' => $affectedstreets
+
+        ]);
     }
 
     /**
@@ -143,9 +151,75 @@ class StatisticsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-    }
+        $validator = Validator::make($request->all(), [
+            'typeOfdisaster' => 'required',
+            'nameOfdisaster' => 'required|max:255',
+            'monthOfdisaster' => 'required',
+            'dayOfdisaster' => 'required',
+            'yearOfdisaster' => 'required|max:2100|integer',
+            'barangay' => 'required',
+            'familiesAffected' => 'required|integer',
+            'individualsAffected' => 'required|integer',
+            'evacuees' => 'required|integer',
+            'addMoreInputFields.*.street' => 'required',
+            'addMoreInputFields.*.families' => 'required|integer',
+        ], $messages = [
+            'typeOfdisaster.required' => 'The type of disaster field is required!',
+            'nameOfdisaster.required' => 'The name of disaster field is required!',
+            'monthOfdisaster.required' => 'The month of disaster field is required!',
+            'dayOfdisaster.required' => 'The day of disaster field is required!',
+            'dayOfdisaster.integer' => 'The day of disaster must be an integer.',
+            'yearOfdisaster.required' => 'The year of disaster field is required!',
+            'yearOfdisaster.max' => 'Enter a valid year!',
+            'barangay.required' => 'The barangay field is required!',
+            'familiesAffected.required' => 'The families affected field is required!',
+            'familiesAffected.integer' => 'Enter a valid number!',
+            'individualsAffected.required' => 'The individuals affected field is required!',
+            'individualsAffected.integer' => 'Enter a valid number!',
+            'evacuees.required' => 'The evacuees field is required!',
+            'evacuees.integer' => 'Enter a valid number!',
+            'addMoreInputFields.*.street.required' => 'The street field is required!',
+            'addMoreInputFields.*.families.required' => 'The number of families field is required!',
+            'addMoreInputFields.*.families.integer' => 'Enter a valid number!',
+        ]);
 
+        if ($validator->fails()) {
+            return redirect('/admin/stats/' . $id . '/edit')
+                ->withErrors($validator)
+                ->withInput();
+        } else {
+
+            DisasterReport::where('id', $id)->update([
+                'type_disaster' => $request->input('typeOfdisaster'),
+                'name_disaster' => $request->input('nameOfdisaster'),
+                'month_disaster' => $request->input('monthOfdisaster'),
+                'day_disaster' =>  $request->input('dayOfdisaster'),
+                'year_disaster' => $request->input('yearOfdisaster'),
+                'barangay' => $request->input('barangay'),
+                'families_affected' => $request->input('familiesAffected'),
+                'individuals_affected' => $request->input('individualsAffected'),
+                'evacuees' => $request->input('evacuees')
+            ]);
+
+            $id = DB::table('disaster_reports')
+                ->where('month_disaster', $request->input('monthOfdisaster'))
+                ->where('day_disaster', $request->input('dayOfdisaster'))
+                ->where('year_disaster', $request->input('yearOfdisaster'))->value('id');
+
+            foreach ($request->addMoreInputFields as $key => $values) {
+
+                DB::table('disaster_affected_streets')->update([
+                    'disaster_id' => $id,
+                    'affected_streets' => $values['street'],
+                    'number_families_affected' => $values['families'],
+                ]);
+            };
+
+
+            return redirect('/admin/stats/')
+                ->with('success', 'Disaster Report Saved!');
+        }
+    }
     /**
      * Remove the specified resource from storage.
      *
@@ -154,6 +228,8 @@ class StatisticsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $disaster = DisasterReport::find($id);
+        $disaster->delete();
+        return redirect('/admin/stats/')->with('success', 'The disaster statistical report has been deleted!');
     }
 }
