a0361234d40e57a323891bdab9d9e136164ff79b
robbychristian <robbychristiandeleon@gmail.com>
2021-11-23 16:17:05 +0800

    Update viewannouncement.blade.php

--- resources/views/features/viewannouncement.blade.php
+++ resources/views/features/viewannouncement.blade.php
@@ -10,13 +10,13 @@
                 <h1 class="h3 mb-4 text-gray-800">Announcements</h1>
             </div>
 
-            <div class="card border-light mb-3">
+            <div class="card border-light mb-3 w-100">
                 <div class="card-header"></div>
                 <div class="card-body">
                     <div class="container">
                         <div class="d-flex flex-column  mb-3">
                             <h2 class="card-title">{{ $announcement->title }}</h2>
-                            <div class="p-2 bd-highlight">{{ $announcement->name }}</div>
+                            <div class="p-2 bd-highlight">Issued by: {{ $announcement->name }}</div>
                             <div class="p-2 bd-highlight">
                                 {{ date('M d, Y \a\t h:m a', strtotime($announcement->created_at)) }}</div>
                         </div>
