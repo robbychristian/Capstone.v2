e2465e2bb1fd2a7257384e113837a45421798f71
robbychristian <robbychristiandeleon@gmail.com>
2021-11-23 14:58:42 +0800

    Update viewannouncement.blade.php

--- resources/views/features/viewannouncement.blade.php
+++ resources/views/features/viewannouncement.blade.php
@@ -14,12 +14,12 @@
                 <div class="card-header"></div>
                 <div class="card-body">
                     <div class="container">
-                        <div class="row">
-                            <div class="col">
-                                <div class="col"><i class="fas fa-user-circle fa-4x" style="color: #DEDEDE"></i>
-                                </div>
+                        <div class="d-flex flex-row align-items-center">
+                            <div class="col-4">
+                                <i class="fas fa-user-circle fa-4x" style="color: #DEDEDE"></i>
                             </div>
-                            <div class="col">
+
+                            <div class="col-8">
                                 <div class="d-flex flex-column bd-highlight mb-3">
                                     <h2 class="card-title">{{ $announcement->title }}</h2>
                                     <div class="p-2 bd-highlight">{{ $announcement->name }}</div>
@@ -28,6 +28,21 @@
                                 </div>
                             </div>
                         </div>
+                        <!--
+                            <div class="row">
+                                <div class="col">
+                                    <div class="col"><i class="fas fa-user-circle fa-4x" style="color: #DEDEDE"></i>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="d-flex flex-column bd-highlight mb-3">
+                                        <h2 class="card-title">{{ $announcement->title }}</h2>
+                                        <div class="p-2 bd-highlight">{{ $announcement->name }}</div>
+                                        <div class="p-2 bd-highlight">
+                                            {{ date('M d, Y \a\t h:m a', strtotime($announcement->created_at)) }}</div>
+                                    </div>
+                                </div>
+                            </div> -->
                     </div>
 
 
