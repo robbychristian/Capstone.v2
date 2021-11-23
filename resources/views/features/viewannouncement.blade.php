c14c5a2800711038e15f08a12da38576c42c6935
robbychristian <robbychristiandeleon@gmail.com>
2021-11-23 14:53:45 +0800

    Update viewannouncement.blade.php

--- resources/views/features/viewannouncement.blade.php
+++ resources/views/features/viewannouncement.blade.php
@@ -13,8 +13,25 @@
             <div class="card border-light mb-3">
                 <div class="card-header"></div>
                 <div class="card-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <div class="col-sm-2 col-md-3 col-lg-2 col-xl-1"><i class="fas fa-user-circle fa-4x"
+                                        style="color: #DEDEDE"></i>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="d-flex flex-column bd-highlight mb-3">
+                                    <h2 class="card-title">{{ $announcement->title }}</h2>
+                                    <div class="p-2 bd-highlight">{{ $announcement->name }}</div>
+                                    <div class="p-2 bd-highlight">
+                                        {{ date('M d, Y \a\t h:m a', strtotime($announcement->created_at)) }}</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                    <h4 class="card-title">{{ $announcement->title }}</h4>
+
                     <p class="card-text">{{ $announcement->body }}</p>
                 </div>
             </div>
