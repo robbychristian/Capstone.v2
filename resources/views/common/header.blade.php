6fc5113d3ddb6559b60ff57c8eeda79d58732096
robbychristian <robbychristiandeleon@gmail.com>
2021-11-24 20:42:37 +0800

    Update header.blade.php

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -34,9 +34,7 @@
                 @if (Auth::user()->user_role === 4)
                     <img class="img-profile rounded-circle"
                         src="{{ URL::asset('KabisigGit/storage/app/public/profile_pics/' . Auth::user()->id . '/' . session('profile_pic')) }}">
-                    @foreach (session('profile_pic') as $user)
-                        <h4>{{ $user }}</h4>
-                    @endforeach
+                    <h4>{{ session('profile_pic')['profile_pic'] }}</h4>
                 @endif
             </a>
             <!-- Dropdown - User Information -->
