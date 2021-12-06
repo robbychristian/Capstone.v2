ed892b14cf7d0cac487ba8880f6f86b678584d72
robbychristian <robbychristiandeleon@gmail.com>
2021-12-06 22:14:14 +0800

    Update guidelines.blade.php

--- resources/views/features/guidelines.blade.php
+++ resources/views/features/guidelines.blade.php
@@ -8,27 +8,29 @@
             <h1 class="h3 mb-0 text-gray-800">Disaster Preparedness</h1>
 
             @if (Auth::user()->user_role === 3)
-                <a href="{{ route('brgy_official.generate.index') }}"
-                    class="d-sm-inline-block btn btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>
-                    Generate Report</a>
+                <a href=""
+                    class="d-sm-inline-block btn btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i>
+                   Add Guidelines </a>
             @elseif (Auth::user()->user_role === 1)
-                <a href="{{ route('admin.generate.index') }}" class="d-sm-inline-block btn btn-primary shadow-sm"><i
-                        class="fas fa-download fa-sm text-white-50"></i>
-                    Generate Report</a>
+                <a href="" class="d-sm-inline-block btn btn-primary shadow-sm"><i
+                        class="fas fa-plus fa-sm text-white-50"></i>
+                        Add Guidelines</a>
             @endif
         </div>
 
         <nav>
             <div class="nav nav-tabs" id="nav-tab" role="tablist">
-              <a class="nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-              <a class="nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-              <a class="nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+              <a class="nav-link active" id="flood-tab" data-toggle="tab" href="#flood" role="tab" aria-controls="flood" aria-selected="true">Flood</a>
+              <a class="nav-link" id="earthquake-tab" data-toggle="tab" href="#earthquake" role="tab" aria-controls="earthquake" aria-selected="false">Earthquake</a>
+              <a class="nav-link" id="tropical-cyclone-tab" data-toggle="tab" href="#tropical-cyclone" role="tab" aria-controls="tropical-cyclone" aria-selected="false">Tropical Cyclone</a>
+              <a class="nav-link" id="tsunami-tab" data-toggle="tab" href="#tsunami" role="tab" aria-controls="tsunami" aria-selected="false">Tsunami</a>
             </div>
           </nav>
           <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
-            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-            <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+            <div class="tab-pane fade show active" id="flood" role="tabpanel" aria-labelledby="flood-tab">Flood</div>
+            <div class="tab-pane fade" id="earthquake" role="tabpanel" aria-labelledby="earthquake-tab">Earthquake</div>
+            <div class="tab-pane fade" id="tropical-cyclone" role="tabpanel" aria-labelledby="tropical-cyclone-tab">Tropical Cyclone</div>
+            <div class="tab-pane fade" id="tsunami" role="tabpanel" aria-labelledby="tsunami-tab">Tsunami</div>
           </div>
 
     </div>
