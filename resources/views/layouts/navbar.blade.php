0f31728a93813ea67a5a3a1e434d1eaf697a93ef
robbychristian <robbychristiandeleon@gmail.com>
2021-12-08 14:08:19 +0800

    Update navbar.blade.php

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,60 +3,9 @@
 <!---------------------->
 
 <!--==========================================================================USER SIDE NAVBAR===============================================================================-->
-@if (Auth::user()->user_role === 2)
-    <div class="d-none d-md-block d-lg-block d-xl-block col-xl-2 col-lg-3 col-md-4">
-        <nav class="navbar navbar-expand-md navbar-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sideNavBar"
-                aria-controls="sideNavBar" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="sideNavBar">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link nav-size" href="/user/account/{{ Auth::user()->id }}/edit"> <i
-                                class="fas fa-user-circle mr-2"></i>Account</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-size" href="{{ route('user.announcements.index') }}"> <i
-                                class="fas fa-bullhorn mr-2"></i></i>Announcements</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link collapsed text-truncate submenu nav-size" href="#submenu1"
-                            data-toggle="collapse" data-target="#submenu1"><i class="fas fa-bookmark mr-2"></i> <span
-                                class="d-none d-sm-inline">Protocols</span></a>
-                        <div class="collapse" id="submenu1" aria-expanded="false">
-                            <ul class="flex-column pl-2 nav">
-                                <li class="nav-item">
-                                    <a class="nav-link ml-3 nav-size"
-                                        href="{{ route('user.guidelines.index') }}">Guidelines</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link ml-3 nav-size"
-                                        href="{{ route('user.evacuation.index') }}">Evacuation
-                                        Centers
-                                        & Hospitals</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-size" href="{{ route('user.vulnerabilitymap.index') }}"><i
-                                class="fas fa-map-marked mr-2"></i>Vulnerability
-                            Map</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-size" href="{{ route('user.reports.index') }}"><i
-                                class="fas fa-edit mr-2"></i>Reports</a>
-                    </li>
-                </ul>
-
-            </div>
-        </nav>
-    </div>
-    <!--==========================================================================BRGY SIDE NAVBAR===============================================================================-->
-@elseif (Auth::user()->user_role >= 3)
+<!--==========================================================================BRGY SIDE NAVBAR===============================================================================-->
+@if (Auth::user()->user_role >= 3)
     <div class="d-none d-md-block d-lg-block d-xl-block col-xl-2 col-lg-3 col-md-4">
         <nav class="navbar navbar-expand-md navbar-light">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sideNavBar"
@@ -110,7 +59,8 @@
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('brgy_official.emergencymessage.create') }}"><i class="fas fa-envelope mr-2"></i>Emergency Alert
+                        <a class="nav-link" href="{{ route('brgy_official.emergencymessage.create') }}"><i
+                                class="fas fa-envelope mr-2"></i>Emergency Alert
                             Message</a>
                     </li>
 
@@ -181,7 +131,8 @@
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.emergencymessage.create') }}"><i class="fas fa-envelope mr-2"></i>Emergency Alert
+                        <a class="nav-link" href="{{ route('admin.emergencymessage.create') }}"><i
+                                class="fas fa-envelope mr-2"></i>Emergency Alert
                             Message</a>
                     </li>
 
@@ -192,8 +143,8 @@
                         <div class="collapse" id="submenu2" aria-expanded="false">
                             <ul class="flex-column pl-2 nav">
                                 <li class="nav-item">
-                                    <a class="nav-link ml-3"
-                                        href="{{ route('admin.manageresident.index') }}">Manage Residents</a>
+                                    <a class="nav-link ml-3" href="{{ route('admin.manageresident.index') }}">Manage
+                                        Residents</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link ml-3"
