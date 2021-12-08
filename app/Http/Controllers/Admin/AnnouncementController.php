a89b72438ecf987b5e9f093c388366e79aa392ec
robbychristian <robbychristiandeleon@gmail.com>
2021-12-08 16:51:26 +0800

    Update AnnouncementController.php

--- app/Http/Controllers/Admin/AnnouncementController.php
+++ app/Http/Controllers/Admin/AnnouncementController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Announcement;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class AnnouncementController extends Controller
 {
@@ -30,7 +31,12 @@ class AnnouncementController extends Controller
      */
     public function create()
     {
-        return view('features.createannouncement');
+        $allBrgys = DB::table('barangays')
+            ->get();
+
+        return view('features.createannouncement', [
+            'barangays' => $allBrgys
+        ]);
     }
 
     /**
