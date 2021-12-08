360b1ab65c137bdef5bbe24e0ea9e37d8ffac204
robbychristian <robbychristiandeleon@gmail.com>
2021-12-08 16:58:25 +0800

    Update AnnouncementController.php

--- app/Http/Controllers/Auth/AnnouncementController.php
+++ app/Http/Controllers/Auth/AnnouncementController.php
@@ -18,7 +18,10 @@ class AnnouncementController extends Controller
      */
     public function index()
     {
-        $announcements = Announcement::where('brgy_loc', '=', Auth::user()->brgy_loc, "&& brgy_loc", '=', 'all')->latest()->paginate(10);
+        $announcements = Announcement::where('brgy_loc', '=', Auth::user()->brgy_loc)
+            ->where('brgy_loc', '=', 'all')
+            ->latest()
+            ->paginate(10);
         return view('features.announcement', [
             'announcements' => $announcements
         ]);
