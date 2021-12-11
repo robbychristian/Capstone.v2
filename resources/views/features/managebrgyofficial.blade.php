d5cadcaeb25322a4d6ac3d283334d4b3239d0f5d
robbychristian <robbychristiandeleon@gmail.com>
2021-12-12 02:16:04 +0800

    Update managebrgyofficial.blade.php

--- resources/views/features/managebrgyofficial.blade.php
+++ resources/views/features/managebrgyofficial.blade.php
@@ -15,44 +15,55 @@
                 class="d-sm-inline-block btn btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i>Add
                 Barangay Official</a>
         </div>
-
-        <div class="table-responsive">
-            <table class="table table-hover">
-                <thead style="background-color: #004f91;">
-                    <tr class="table-active">
-                        <th scope="col" style="color: white;">Full Name</th>
-                        <th scope="col" style="color: white;">Email</th>
-                        <th scope="col" style="color: white;">Contact Number</th>
-                        <th scope="col" style="color: white;">Barangay Location</th>
-                        <th scope="col" style="color: white;">Barangay Position</th>
-                        <th scope="col" style="color: white;">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($brgy_officials as $brgy_official)
-                        <tr>
-                            <td>{{ $brgy_official->first_name }} {{ $brgy_official->middle_name }}
-                                {{ $brgy_official->last_name }}</td>
-                            <td>{{ $brgy_official->email }}</td>
-                            <td>{{ $brgy_official->contact_no }}</td>
-                            <td>{{ $brgy_official->brgy_loc }}</td>
-                            <td>
-                                <form action="/admin/managebrgy_official/{{ $brgy_official->id }}" method="POST">
-                                    @csrf
-                                    @method('delete')
-                                    <button class="btn btn-danger">Delete</button>
-                                </form>
-                                <form action="/admin/managebrgy_official/demote/{{ $brgy_official->id }}" method="POST">
-                                    @csrf
-                                    @method('post')
-                                    <button class="btn btn-danger">Demote</button>
-                                </form>
-                            </td>
+        @if (count($brgy_officials) > 0)
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead style="background-color: #004f91;">
+                        <tr class="table-active">
+                            <th scope="col" style="color: white;">Full Name</th>
+                            <th scope="col" style="color: white;">Email</th>
+                            <th scope="col" style="color: white;">Contact Number</th>
+                            <th scope="col" style="color: white;">Barangay Location</th>
+                            <th scope="col" style="color: white;">Barangay Position</th>
+                            <th scope="col" style="color: white;">Action</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        @foreach ($brgy_officials as $brgy_official)
+                            <tr>
+                                <td>{{ $brgy_official->first_name }} {{ $brgy_official->middle_name }}
+                                    {{ $brgy_official->last_name }}</td>
+                                <td>{{ $brgy_official->email }}</td>
+                                <td>{{ $brgy_official->contact_no }}</td>
+                                <td>{{ $brgy_official->brgy_loc }}</td>
+                                <td>
+                                    <form action="/admin/managebrgy_official/{{ $brgy_official->id }}" method="POST">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-danger">Delete</button>
+                                    </form>
+                                    <form action="/admin/managebrgy_official/demote/{{ $brgy_official->id }}"
+                                        method="POST">
+                                        @csrf
+                                        @method('post')
+                                        <button class="btn btn-danger">Demote</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+        @else
+            <div class="card mt-3">
+                <div class="card-body" style="font-weight: 400; font-size: 1rem;">
+                    There are no registered users.
+                </div>
+            </div>
+        @endif
     </div>
+
+
 
 @endsection
