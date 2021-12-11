8bbfc6a6202172de741ca626bcd61b6843360c36
robbychristian <robbychristiandeleon@gmail.com>
2021-12-11 18:01:48 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -120,10 +120,10 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::view('/home', 'dashboard.admin.home')->name('home');
         Route::view('/create', 'dashboard.admin.register_brgy')->name('register_brgy');
         Route::resource('/announcements', AdminAnnouncement::class);
-        //Route::resource('/pending', AdminPendingAnnouncements::class);
+        Route::resource('/pending', AdminPendingAnnouncements::class);
         //Route::view('/announcements/pending', 'features.pendingannouncements')->name('announcement.pending');
         //Route::resource('/announcements/pending', AdminPendingAnnouncements::class);
-        Route::get('/announcements/pending', [AdminAnnouncement::class, 'pending'])->name('announcement.pending');
+        //Route::get('/announcements/pending', [AdminAnnouncement::class, 'pending'])->name('announcement.pending');
         Route::post('/announcements/approve/{id}', [AdminAnnouncement::class, 'approve'])->name('announcement.approve');
         Route::post('/announcements/disapprove/{id}', [AdminAnnouncement::class, 'disapprove'])->name('announcement.disapprove');
         Route::resource('/evacuation', AdminEvacuation::class);
