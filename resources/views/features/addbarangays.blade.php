866be322834be4afb636f688fa3e06d44b04f29a
robbychristian <robbychristiandeleon@gmail.com>
2021-12-12 16:46:55 +0800

    Update addbarangays.blade.php

--- resources/views/features/addbarangays.blade.php
+++ resources/views/features/addbarangays.blade.php
@@ -38,27 +38,22 @@
                     label: data[4],
                     icon: data[3] == "1" ? is_added_marker : is_not_added_marker,
                     html: '<h5>' + data[2] + '</h5> ' +
-                        @if (data[3] === 1)
-                            '<h6><span class="badge badge-success">Added</span></h6>' +
-                        @else
-                            '<h6><span class="badge badge-danger">Not Added</span></h6>' +
-                        @endif
 
-                    '<div class="d-grid gap-2 d-md-flex justify-content-md-center">' +
+                        '<div class="d-grid gap-2 d-md-flex justify-content-md-center">' +
 
-                    '<form action="/admin/managebarangay/addbarangay/' + data[4] + '" method="POST">' +
-                    '@csrf' +
-                    '@method("POST")' +
-                    '<button class="btn btn-success mr-3">Add</button>' +
-                    '</form>' +
+                        '<form action="/admin/managebarangay/addbarangay/' + data[4] + '" method="POST">' +
+                        '@csrf' +
+                        '@method("POST")' +
+                        '<button class="btn btn-success mr-3">Add</button>' +
+                        '</form>' +
 
-                    '<form action="/admin/managebarangay/deletebarangay/' + data[4] + '" method="POST">' +
-                    '@csrf' +
-                    '@method("POST")' +
-                    '<button class="btn btn-warning">Archive</button>' +
-                    '</form>' +
+                        '<form action="/admin/managebarangay/deletebarangay/' + data[4] + '" method="POST">' +
+                        '@csrf' +
+                        '@method("POST")' +
+                        '<button class="btn btn-warning">Archive</button>' +
+                        '</form>' +
 
-                    '</div>'
+                        '</div>'
 
 
                 });
