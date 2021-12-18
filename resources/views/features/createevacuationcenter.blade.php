b9a76fa83deae474cfa4c1677c15b5632184a170
robbychristian <robbychristiandeleon@gmail.com>
2021-12-18 21:36:48 +0800

    Update createevacuationcenter.blade.php

--- resources/views/features/createevacuationcenter.blade.php
+++ resources/views/features/createevacuationcenter.blade.php
@@ -122,7 +122,9 @@
                                         {{ $message }}
                                     @enderror</small>
                             </div>
-
+                            <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title"
+                                data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle
+                                popover</button>
 
                             <div class="form-group">
                                 <div class="d-flex flex-row">
