5a3155899db37e3069746e05ba0d237a92bb18e0
robbychristian <robbychristiandeleon@gmail.com>
2021-12-20 20:09:55 +0800

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,9 +21,6 @@
     <!-- Core plugin JavaScript-->
     <script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('admin/js/sb-admin-2.min.js') }}"></script>
-
     <!--- icon -->
     <link rel="icon" href="{{ asset('img/title-website-icon.png') }}" type="image/png" sizes="16x16">
 
@@ -59,8 +56,7 @@
     <!-- Data tables -->
     <!-- Page level plugins -->
     <script src="{{ asset('admin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
-    <!--
-    <script src="{{ asset('admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script> -->
+    <script src="{{ asset('admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
 
     <style>
@@ -367,6 +363,9 @@
         </div>
     </div>
 
+    
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('admin/js/sb-admin-2.min.js') }}"></script>
 
 
 
