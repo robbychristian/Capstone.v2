76495656679a1f9978a9620f440cd08832c96451
robbychristian <robbychristiandeleon@gmail.com>
2021-12-21 20:13:44 +0800

    Update guidelines.blade.php

--- resources/views/features/guidelines.blade.php
+++ resources/views/features/guidelines.blade.php
@@ -49,8 +49,25 @@
                                     @if ($guideline->disaster == 'Flood')
                                         @if ($guideline->time == 'Before')
 
+                                            <li class="list-group-item bg-transparent">
+                                                <div class="card border-left-primary shadow h-100 py-2">
+                                                    <div class="card-body">
+                                                        <div class="row no-gutters align-items-center">
+                                                            <div class="col mr-2">
+                                                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                                    <i
+                                                                    class="fas fa-angle-double-right mr-2"
+                                                                    style="color: #004F91"></i> {{ $guideline->guideline }}
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-auto">
+                                                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
 
-                                            <li class="list-group-item bg-transparent">{{ $guideline->guideline }}</li>
+                                            </li>
                                         @endif
                                     @endif
                                 @endforeach
