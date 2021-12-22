8e13a5915e7c98a6d00a6f6f64366780c8e9214b
robbychristian <robbychristiandeleon@gmail.com>
2021-12-22 18:54:46 +0800

    Update evacuationcenter.blade.php

--- resources/views/features/evacuationcenter.blade.php
+++ resources/views/features/evacuationcenter.blade.php
@@ -121,13 +121,13 @@
                     map: allmap,
                     label: data[3],
                     icon: data[2] == "1" ? is_added_marker : is_not_added_marker,
-                    //html: '<ul class="list-group list-group-flush">' +
-                    //    '<li class="list-group-item"><i class="fas fa-directions mr-2 color"></i>Nearest Landmark:'+ data[5]'</li>' +
-                    //    '<li class="list-group-item"><i class="fas fa-phone-square-alt mr-2 color"></i>Contact Number:'+ data[6]'</li>' +
-                    //    '<li class="list-group-item"><i class="fas fa-users mr-2 color"></i>Capacity'+ data[7]'</li>' +
-                    //    '<li class="list-group-item"></li>' +
-                    //    '<li class="list-group-item"></li>' +
-                    //    '</ul>',
+                    html: '<ul class="list-group list-group-flush">' +
+                        '<li class="list-group-item"><i class="fas fa-directions mr-2 color"></i>Nearest Landmark:' + data[5] + '</li>' +
+                        '<li class="list-group-item"><i class="fas fa-phone-square-alt mr-2 color"></i>Contact Number:' + data[6] + '</li>' +
+                        '<li class="list-group-item"><i class="fas fa-users mr-2 color"></i>Capacity:'+ data[7] + '</li>' +
+                        '<li class="list-group-item"></li>' +
+                        '<li class="list-group-item"></li>' +
+                        '</ul>',
                 });
 
 
