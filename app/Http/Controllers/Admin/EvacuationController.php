d1c43ab4e4a2270c66609fd71bb757501c9191e1
robbychristian <robbychristiandeleon@gmail.com>
2021-12-22 18:02:07 +0800

    Update EvacuationController.php

--- app/Http/Controllers/Admin/EvacuationController.php
+++ app/Http/Controllers/Admin/EvacuationController.php
@@ -84,7 +84,7 @@ class EvacuationController extends Controller
             ->where('is_added', 1)
             ->get();
         $evacuationcenters = EvacuationCenters::where('deleted_at', null)->paginate(2);
-        $evacmaps = EvacuationCenters::all();
+        $evacmaps = EvacuationCenters::all()->where('deleted_at', null);
         return view('features.evacuationcenter', [
             'evacuationcenters' => $evacuationcenters,
             'barangays' => $barangays,
