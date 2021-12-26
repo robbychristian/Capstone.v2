c01bf6350487cee3678a04986ca6e8117d56c6ce
robbychristian <robbychristiandeleon@gmail.com>
2021-12-26 22:54:27 +0800

    Update viewresident.blade.php

--- resources/views/features/viewresident.blade.php
+++ resources/views/features/viewresident.blade.php
@@ -99,13 +99,46 @@
 
                                 <div class="col-sm-9">
                                     @if ($user->is_valid == 1)
-                                        <i class="fas fa-check-circle text-success mr-2"></i><span
-                                            class="text-success font-weight-bold">Verified Resident</span>
+                                        <i class="fas fa-check-circle text-success mr-2"></i>
+                                        <span class="text-success font-weight-bold">Verified Resident</span>
 
                                     @else
-                                        <i class="fas fa-times-circle text-danger mr-2"></i><span
-                                            class="text-danger font-weight-bold">Unverified Resident</span>
+                                        <i class="fas fa-times-circle text-danger mr-2"></i>
+                                        <span class="text-danger font-weight-bold">Unverified Resident</span>
                                     @endif
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="content mb-2">
+                            <div class="row">
+                                <div class="col-sm-3 ">Valid ID</div>
+                                <div class="col-sm-9 ">
+                                    <!-- Button trigger modal -->
+
+                                    <a data-toggle="modal" data-target="#id{{ $user->id }}">View Photo</a>
+                                
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="id{{ $user->id }}" tabindex="-1"
+                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                        aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <img src="{{ URL::asset('KabisigGit/storage/app/public/profile_pics/' . $user->id . '/' . $profile->profile_pic) }}"
+                                                        class="img-responsive"
+                                                        style="width: 100%; object-fit: cover; height: 300px;">
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
