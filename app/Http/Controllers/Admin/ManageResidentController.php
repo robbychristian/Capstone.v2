7330d7f650be34691ada31b890c11350a9ba8e71
robbychristian <robbychristiandeleon@gmail.com>
2021-12-26 17:19:24 +0800

    Update ManageResidentController.php

--- app/Http/Controllers/Admin/ManageResidentController.php
+++ app/Http/Controllers/Admin/ManageResidentController.php
@@ -85,7 +85,7 @@ class ManageResidentController extends Controller
                     <img class="mr-3 float-left rounded-circle" width="50" height="50" src="'.\URL::asset('KabisigGit/storage/app/public/profile_pics/'. $row->id . '/' . $row->profile_pic) .'">
                     <div class="media-body">
                       <h6 class="mt-0">'.$row->first_name.' '.$row->middle_name.' '.$row->last_name.'</h6>
-                        <small class="text-muted">'.$row->user_email.'</small>
+                      <span class=text-muted style=font-size:"smaller;">'.$row->user_email.'</span>
                     </div>
                   </div>';
                    
