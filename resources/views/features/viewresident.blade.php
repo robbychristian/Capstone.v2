a357f308114558a3634b4fca1d22100574bd6a43
robbychristian <robbychristiandeleon@gmail.com>
2021-12-26 20:01:01 +0800

    Update viewresident.blade.php

--- resources/views/features/viewresident.blade.php
+++ resources/views/features/viewresident.blade.php
@@ -16,11 +16,8 @@
             <div class="card-body">
                 <div class="row d-flex">
                     <div class="col-sm-12 col-md-6 col-lg-4 align-items-center">
-                        <div class="profile-pic" style="height: 300px; width: 300px; overflow: hidden;">
-                            <img src="{{ URL::asset('KabisigGit/storage/app/public/profile_pics/' . $user->id . '/' . $profile->profile_pic) }}"
-                                class="rounded-circle img-responsive" style="height: 300px;">
-                        </div>
-
+                        <img src="{{ URL::asset('KabisigGit/storage/app/public/profile_pics/' . $user->id . '/' . $profile->profile_pic) }}"
+                            class="img-responsive" style="width: 100%; object-fit: cover; height: 300px;">
                         <h4> {{ $user->first_name }} {{ $profile->middle_name }} {{ $user->last_name }}</h4>
                     </div>
                     <div class="col-sm-12 col-md-6 col-lg-8">
