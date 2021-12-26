e54ca9ab976aeef15bbb9b47772ff1fcb7a22aec
robbychristian <robbychristiandeleon@gmail.com>
2021-12-26 19:38:13 +0800

    Update viewresident.blade.php

--- resources/views/features/viewresident.blade.php
+++ resources/views/features/viewresident.blade.php
@@ -14,15 +14,18 @@
 
         <div class="card shadow-card mb-3 mt-3">
             <div class="card-body">
-              <div class="row">
-                  <div class="col-sm-12 col-md-6 col-lg-4">
-                    <img src="{{ URL::asset('KabisigGit/storage/app/public/profile_pics/' . $user->id . '/' . $profile->profile_pic) }}" width="300" height="300" class="rounded-circle mx-auto d-block" alt="...">
-                    <h6> {{ $user->first_name }} {{ $profile->middle_name }} {{ $user->last_name }}</h6>
+                <div class="row">
+                    <div class="col-sm-12 col-md-6 col-lg-4">
+                        <img src="{{ URL::asset('KabisigGit/storage/app/public/profile_pics/' . $user->id . '/' . $profile->profile_pic) }}"
+                            class="rounded-circle mx-auto d-block img-responsive" style="width: 100%;
+                         object-fit: cover;
+                         height: 300px;">
+                        <h4> {{ $user->first_name }} {{ $profile->middle_name }} {{ $user->last_name }}</h4>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-8">
+
+                    </div>
                 </div>
-                  <div class="col-sm-12 col-md-6 col-lg-8">
-                     
-                  </div>
-              </div>
             </div>
         </div>
 
