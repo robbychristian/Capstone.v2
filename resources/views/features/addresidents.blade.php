73734c5ab24861abcbfb2a615d41707a5e47f823
robbychristian <robbychristiandeleon@gmail.com>
2021-12-28 15:11:25 +0800

    test

--- resources/views/features/addresidents.blade.php
+++ resources/views/features/addresidents.blade.php
@@ -143,184 +143,191 @@
 
     <div class="container-fluid" style="color: black;">
         @if (Auth::user()->user_role === 1)
-            <a class="btn btn-primary btn-sm" href="/admin/manageresident" role="button">Back</a>
+            <a class="btn btn-primary btn-sm mb-3" href="/admin/manageresident" role="button">Back</a>
         @elseif (Auth::user()->user_role === 3)
-            <a class="btn btn-primary btn-sm" href="/brgy_official/manageresident" role="button">Back</a>
+            <a class="btn btn-primary btn-sm mb-3" href="/brgy_official/manageresident" role="button">Back</a>
         @endif
 
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Add Resident</h1>
         </div>
 
-        <small class="text-muted mb-5">Fields marked with an <span class="text-danger" style="font-size: 1rem">*</span> are
-            required.</small>
-
-        @if (Auth::user()->user_role === 1)
-            <form action="{{ route('admin.manageresident.store') }}" method="POST" class="mt-5"
-                enctype="multipart/form-data">
-            @elseif (Auth::user()->user_role === 3)
-                <form action="{{ route('brgy_official.manageresident.store') }}" method="POST" class="mt-5"
-                    enctype="multipart/form-data">
-        @endif
-        @csrf
-        <div class="form-row">
-            <div class="form-group col-md-4 required">
-                <label class="control-label" for="inputfName">First Name</label>
-                <input name="fname" type="text" class="form-control" id="inputfName" value={{ old('fname') }}>
-                <small class="text-danger">@error('fname')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-            <div class="form-group col-md-4 required">
-                <label class="control-label" for="inputmName">Middle Name</label>
-                <input name="mname" type="text" class="form-control" id="inputmName" value={{ old('mname') }}>
-                <small class="text-danger">@error('mname')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-
-            <div class="form-group col-md-4 required">
-                <label class="control-label" for="inputlName">Last Name</label>
-                <input name="lname" type="text" class="form-control" id="inputlName" value={{ old('lname') }}>
-                <small class="text-danger">@error('lname')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-8 required">
-                <label class="control-label" for="inputAddress">Home Address</label>
-                <input name="address" type="text" class="form-control" id="inputAddress" value={{ old('address') }}>
-                <small class="text-danger">@error('address')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-            <div class="form-group col-md-4 required">
-                <label class="control-label" for="inputBrgy">Barangay</label>
+        <div class="card shadow mb-3 mt-3">
+            <div class="card-body">
+                <small class="text-muted mb-5">Fields marked with an <span class="text-danger"
+                        style="font-size: 1rem">*</span> are
+                    required.</small>
                 @if (Auth::user()->user_role === 1)
-                    <select name="brgy" id="inputBrgy" class="form-control">
-                        <option value="">Barangay</option>
-                        <option value='Barangay Dela Paz'>Barangay Dela Paz</option>
-                        <option value='Barangay Manggahan'>Barangay Manggahan</option>
-                        <option value='Barangay Maybunga'>Barangay Maybunga</option>
-                        <option value='Barangay Rosario'>Barangay Rosario</option>
-                        <option value='Barangay Santolan'>Barangay Santolan</option>
-                    </select>
-                    <small class="text-danger">@error('brgy')
+                    <form action="{{ route('admin.manageresident.store') }}" method="POST" class="mt-5"
+                        enctype="multipart/form-data">
+                    @elseif (Auth::user()->user_role === 3)
+                        <form action="{{ route('brgy_official.manageresident.store') }}" method="POST"
+                            class="mt-5" enctype="multipart/form-data">
+                @endif
+                @csrf
+                <div class="form-row">
+                    <div class="form-group col-md-4 required">
+                        <label class="control-label" for="inputfName">First Name</label>
+                        <input name="fname" type="text" class="form-control" id="inputfName" value={{ old('fname') }}>
+                        <small class="text-danger">@error('fname')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+                    <div class="form-group col-md-4 required">
+                        <label class="control-label" for="inputmName">Middle Name</label>
+                        <input name="mname" type="text" class="form-control" id="inputmName" value={{ old('mname') }}>
+                        <small class="text-danger">@error('mname')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+
+                    <div class="form-group col-md-4 required">
+                        <label class="control-label" for="inputlName">Last Name</label>
+                        <input name="lname" type="text" class="form-control" id="inputlName" value={{ old('lname') }}>
+                        <small class="text-danger">@error('lname')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-8 required">
+                        <label class="control-label" for="inputAddress">Home Address</label>
+                        <input name="address" type="text" class="form-control" id="inputAddress"
+                            value={{ old('address') }}>
+                        <small class="text-danger">@error('address')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+                    <div class="form-group col-md-4 required">
+                        <label class="control-label" for="inputBrgy">Barangay</label>
+                        @if (Auth::user()->user_role === 1)
+                            <select name="brgy" id="inputBrgy" class="form-control">
+                                <option value="">Barangay</option>
+                                <option value='Barangay Dela Paz'>Barangay Dela Paz</option>
+                                <option value='Barangay Manggahan'>Barangay Manggahan</option>
+                                <option value='Barangay Maybunga'>Barangay Maybunga</option>
+                                <option value='Barangay Rosario'>Barangay Rosario</option>
+                                <option value='Barangay Santolan'>Barangay Santolan</option>
+                            </select>
+                            <small class="text-danger">@error('brgy')
+                                    {{ $message }}
+                                @enderror</small>
+                        @elseif (Auth::user()->user_role === 3)
+                            <input name="brgy" type="text" class="form-control" id="inputAddress"
+                                value="{{ Auth::user()->brgy_loc }}" readonly>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group col-md-6 required">
+                        <label class="control-label" for="inputContactNum">Contact Number</label>
+                        <input name="cnum" type="text" class="form-control" id="inputContactNum"
+                            onkeypress="return onlyNumberKey(event)" maxlength="11" value={{ old('cnum') }}>
+                        <small class="text-danger">@error('cnum')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+                    <div class="form-group col-md-2 required">
+                        <label class="control-label" for="inputBday">Birthday</label>
+                        <select name="mbday" id="mbday" class="form-control" value="{{ old('mbday') }}">
+                            <option selected disabled>Month</option>
+                            <option value='1'>January</option>
+                            <option value='2'>February</option>
+                            <option value='3'>March</option>
+                            <option value='4'>April</option>
+                            <option value='5'>May</option>
+                            <option value='6'>June</option>
+                            <option value='7'>July</option>
+                            <option value='8'>August</option>
+                            <option value='9'>September</option>
+                            <option value='10'>October</option>
+                            <option value='11'>November</option>
+                            <option value='12'>December</option>
+                        </select>
+                        <small class="text-danger">@error('mbday')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+                    <div class="form-group col-md-2">
+                        <div class="d-none d-xl-block d-lg-block d-md-block">
+                            <label for="inputBday" style="color:white">asdasd</label>
+                        </div>
+                        <select name="dbday" id="dbday" class="form-control" value="{{ old('dbday') }}">
+                            <option selected disabled>Day</option>
+                        </select>
+                        <small class="text-danger">@error('dbday')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+                    <div class="form-group col-md-2">
+                        <div class="d-none d-xl-block d-lg-block d-md-block">
+                            <label for="inputBday" style="color:white"></label>
+                        </div>
+                        <input name="ybday" type="text" class="form-control mt-2" id="inputBday" placeholder="Year"
+                            value="{{ old('ybday') }}">
+                        <small class="text-danger">@error('ybday')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+                </div>
+
+                <div class="form-group required">
+                    <label class="control-label" for="inputEmail">Email</label>
+                    <input name="email" type="email" class="form-control" id="inputEmail" value={{ old('email') }}>
+                    <small class="text-danger">@error('email')
                             {{ $message }}
                         @enderror</small>
-                @elseif (Auth::user()->user_role === 3)
-                    <input name="brgy" type="text" class="form-control" id="inputAddress"
-                        value="{{ Auth::user()->brgy_loc }}" readonly>
-                @endif
-            </div>
-        </div>
-
-        <div class="form-row">
-            <div class="form-group col-md-6 required">
-                <label class="control-label" for="inputContactNum">Contact Number</label>
-                <input name="cnum" type="text" class="form-control" id="inputContactNum"
-                    onkeypress="return onlyNumberKey(event)" maxlength="11" value={{ old('cnum') }}>
-                <small class="text-danger">@error('cnum')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-            <div class="form-group col-md-2 required">
-                <label class="control-label" for="inputBday">Birthday</label>
-                <select name="mbday" id="mbday" class="form-control" value="{{ old('mbday') }}">
-                    <option selected disabled>Month</option>
-                    <option value='1'>January</option>
-                    <option value='2'>February</option>
-                    <option value='3'>March</option>
-                    <option value='4'>April</option>
-                    <option value='5'>May</option>
-                    <option value='6'>June</option>
-                    <option value='7'>July</option>
-                    <option value='8'>August</option>
-                    <option value='9'>September</option>
-                    <option value='10'>October</option>
-                    <option value='11'>November</option>
-                    <option value='12'>December</option>
-                </select>
-                <small class="text-danger">@error('mbday')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-            <div class="form-group col-md-2">
-                <div class="d-none d-xl-block d-lg-block d-md-block">
-                    <label for="inputBday" style="color:white">asdasd</label>
                 </div>
-                <select name="dbday" id="dbday" class="form-control" value="{{ old('dbday') }}">
-                    <option selected disabled>Day</option>
-                </select>
-                <small class="text-danger">@error('dbday')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-            <div class="form-group col-md-2">
-                <div class="d-none d-xl-block d-lg-block d-md-block">
-                    <label for="inputBday" style="color:white"></label>
+
+                <div class="form-row">
+                    <div class="form-group col-md-6 required">
+                        <label class="control-label" for="inputPassword">Password</label>
+                        <input name="pass" type="password" class="form-control" id="inputPassword">
+                        <small class="text-muted">Must be 8 and above characters long.</small>
+                        <small class="text-danger">@error('pass')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
+                    <div class="form-group col-md-6 required">
+                        <label class="control-label" for="inputConfpw">Confirm Password</label>
+                        <input name="conf_pass" type="password" class="form-control" id="inputConfpw">
+                        <small class="text-danger">@error('conf_pass')
+                                {{ $message }}
+                            @enderror</small>
+                    </div>
                 </div>
-                <input name="ybday" type="text" class="form-control mt-2" id="inputBday" placeholder="Year"
-                    value="{{ old('ybday') }}">
-                <small class="text-danger">@error('ybday')
-                        {{ $message }}
-                    @enderror</small>
+
+                <div class="form-group required">
+                    <label class="control-label" for="inputUpload">Upload your Profle Picture</label>
+                    <input name="file" class="form-control" type="file" id="formFile">
+                    <small class="text-muted">Accessible formats: jpg, png, jpeg,
+                        only</small>
+                    @error('file')
+                        <small class="text-danger">
+                            {{ $message }}
+                        </small>
+                    @enderror
+                </div>
+
+                <div class="form-group form-check required">
+                    <input name="cbox" type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label control-label" for="exampleCheck1">I have read and agree to the <span
+                            data-toggle="modal" data-target="#exampleModal"
+                            style="text-decoration: underline; color: blue; cursor: pointer;">terms and
+                            conditions
+                        </span></label>
+                    <small class="text-danger">@error('cbox')
+                            {{ $message }}
+                        @enderror</small>
+                </div>
+
+                <button name="" type="submit" class="btn btn-primary mt-2">Submit</button>
+                </form>
             </div>
+
         </div>
 
-        <div class="form-group required">
-            <label class="control-label" for="inputEmail">Email</label>
-            <input name="email" type="email" class="form-control" id="inputEmail" value={{ old('email') }}>
-            <small class="text-danger">@error('email')
-                    {{ $message }}
-                @enderror</small>
-        </div>
-
-        <div class="form-row">
-            <div class="form-group col-md-6 required">
-                <label class="control-label" for="inputPassword">Password</label>
-                <input name="pass" type="password" class="form-control" id="inputPassword">
-                <small class="text-muted">Must be 8 and above characters long.</small>
-                <small class="text-danger">@error('pass')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-            <div class="form-group col-md-6 required">
-                <label class="control-label" for="inputConfpw">Confirm Password</label>
-                <input name="conf_pass" type="password" class="form-control" id="inputConfpw">
-                <small class="text-danger">@error('conf_pass')
-                        {{ $message }}
-                    @enderror</small>
-            </div>
-        </div>
-
-        <div class="form-group required">
-            <label class="control-label" for="inputUpload">Upload your Profle Picture</label>
-            <input name="file" class="form-control" type="file" id="formFile">
-            <small class="text-muted">Accessible formats: jpg, png, jpeg,
-                only</small>
-            @error('file')
-                <small class="text-danger">
-                    {{ $message }}
-                </small>
-            @enderror
-        </div>
-
-        <div class="form-group form-check required">
-            <input name="cbox" type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label control-label" for="exampleCheck1">I have read and agree to the <span
-                    data-toggle="modal" data-target="#exampleModal"
-                    style="text-decoration: underline; color: blue; cursor: pointer;">terms and
-                    conditions
-                </span></label>
-            <small class="text-danger">@error('cbox')
-                    {{ $message }}
-                @enderror</small>
-        </div>
-
-        <button name="" type="submit" class="btn btn-primary mt-2">Submit</button>
-        </form>
     </div>
 
     <!-- Modal -->
--- resources/views/features/viewresident.blade.php
+++ resources/views/features/viewresident.blade.php
@@ -16,7 +16,7 @@
             <h1 class="h3 mb-0 text-gray-800">Resident Profile</h1>
         </div>
 
-        <div class="card shadow-card mb-3 mt-3">
+        <div class="card shadow mb-3 mt-3">
             <div class="card-body">
                 <div class="row justify-content-center">
                     <div class="col-sm-12 d-flex" style="justify-content: center; align-items: center;">
