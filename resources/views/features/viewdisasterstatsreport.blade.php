9d2abe9b26b4789257c113b4d3f7c28cebc95bb7
robbychristian <robbychristiandeleon@gmail.com>
2021-12-30 18:56:29 +0800

    Update viewdisasterstatsreport.blade.php

--- resources/views/features/viewdisasterstatsreport.blade.php
+++ resources/views/features/viewdisasterstatsreport.blade.php
@@ -19,14 +19,14 @@
                                     <div class="row">
                                         <div class="col-sm-12 col-md-12 col-lg-6">
                                             <div class="row">
-                                                <div class="col-sm-4 col-md-4 col-lg-3" style="font-weight: 500;">Type:</div>
-                                                <div class="col-sm-8 col-md-8 col-lg-9"> {{ $disasterstats->type_disaster }}</div>
+                                                <div class="col-sm-4 col-md-4" style="font-weight: 500;">Type:</div>
+                                                <div class="col-sm-8 col-md-8"> {{ $disasterstats->type_disaster }}</div>
                                             </div>
                                         </div>
                                         <div class="col-sm-12 col-md-6">
                                             <div class="row">
-                                                <div class="col-sm-4 col-lg-3" style="font-weight: 500;">Name:</div>
-                                                <div class="col-sm-8 col-lg-9"> {{ $disasterstats->name_disaster }}</div>
+                                                <div class="col-sm-4" style="font-weight: 500;">Name:</div>
+                                                <div class="col-sm-8"> {{ $disasterstats->name_disaster }}</div>
                                             </div>
                                         </div>
                                     </div>
@@ -36,8 +36,8 @@
                                     <div class="row">
                                         <div class="col-sm-12 col-md-12 col-lg-6">
                                             <div class="row">
-                                                <div class="col-sm-4 col-md-4 col-lg-3" style="font-weight: 500;">Date:</div>
-                                                <div class="col-sm-8 col-md-8 col-lg-9"> {{ $disasterstats->month_disaster }}
+                                                <div class="col-sm-4 col-md-4" style="font-weight: 500;">Date:</div>
+                                                <div class="col-sm-8 col-md-8"> {{ $disasterstats->month_disaster }}
                                                     {{ $disasterstats->day_disaster }}, {{ $disasterstats->year_disaster }}</div>
                                             </div>
                                         </div>
