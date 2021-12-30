aacb538081094115838db4b7a62150b946a3d3ea
robbychristian <robbychristiandeleon@gmail.com>
2021-12-30 17:46:08 +0800

    Update viewdisasterstatsreport.blade.php

--- resources/views/features/viewdisasterstatsreport.blade.php
+++ resources/views/features/viewdisasterstatsreport.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', '| Disaster Statistical Reports')
 @section('content')
+    <!-- comments: fix design // continue fixing edit report // fix generate report -->
     <div class="container-fluid" style="color: black;">
         <a href="{{ url()->previous() }}" class="btn btn-primary btn-sm active mb-3" role="button"
             aria-pressed="true">Back</a>
@@ -9,68 +10,78 @@
         <div class="card shadow-card mb-3 mt-3">
             <div class="card-body">
 
-                {{ $disasterstats->id }}
-
-                {{ $disasterstats->type_disaster }}
-
-                <!-- comments: fix design // continue fixing edit report // fix generate report -->
-                <!-- Content Row -->
                 <div class="row">
-                    <div class="col-xl-4 mb-4">
-                        <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="col-6"> 
+                        <div class="card">
                             <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                            Families Affected</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                            {{ $disasterstats->families_affected }}</div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-home fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
+                                {{ $disasterstats->type_disaster }}
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-4 mb-4">
-                        <div class="card border-left-info shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                            Individuals Affected</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                            {{ $disasterstats->individuals_affected }}</div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-male fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-xl-4 mb-4">
-                        <div class="card border-left-success shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                            Evacuees</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $disasterstats->evacuees }}
+                    <div class="col-6">
+                        <!-- Content Row -->
+                        <div class="row">
+                            <div class="col-xl-4 mb-4">
+                                <div class="card border-left-primary shadow h-100 py-2">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                                    Families Affected</div>
+                                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                    {{ $disasterstats->families_affected }}</div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <i class="fas fa-home fa-2x text-gray-300"></i>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-house-user fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                            <div class="col-xl-4 mb-4">
+                                <div class="card border-left-info shadow h-100 py-2">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                                    Individuals Affected</div>
+                                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                    {{ $disasterstats->individuals_affected }}</div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <i class="fas fa-male fa-2x text-gray-300"></i>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+
+                            <div class="col-xl-4 mb-4">
+                                <div class="card border-left-success shadow h-100 py-2">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                                    Evacuees</div>
+                                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                    {{ $disasterstats->evacuees }}
+                                                </div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <i class="fas fa-house-user fa-2x text-gray-300"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
-
-
                 </div>
+
+
+
 
                 <div class="table-responsive">
                     <table class="table " style="color: black;">
