d8bed8c9566c34857b1aff683f9d55d08565a40f
robbychristian <robbychristiandeleon@gmail.com>
2022-01-03 13:45:54 +0800

    Update editannouncement.blade.php

--- resources/views/features/editannouncement.blade.php
+++ resources/views/features/editannouncement.blade.php
@@ -5,8 +5,8 @@
         <div class="container-fluid">
             @if (Auth::user()->user_role === 1)
                 <form action="/admin/announcements/{{ $announcement->id }}" method="POST">
-                @elseif (Auth::user()->user_role === 3)
-                    <form action="/brgy_official/announcements/{{ $announcement->id }}" method="POST">
+                @elseif (Auth::user()->user_role >= 3)
+                    <form action="/user/announcements/{{ $announcement->id }}" method="POST">
             @endif
             @csrf
             @method('PUT')
@@ -25,6 +25,16 @@
                         {{ $message }}
                     @enderror</small>
             </div>
+
+            @if (Auth::user()->user_role == 1)
+                <a class="btn btn-outline-secondary float-right" href="{{ route('admin.announcements.index') }}"
+                    role="button">Cancel</a>
+
+            @elseif (Auth::user()->user_role >= 3)
+                <a class="btn btn-outline-secondary float-right" href="{{ route('user.announcements.index') }}"
+                    role="button">Cancel</a>
+            @endif
+
             <button class="btn btn-primary float-right ">Post</button>
             </form>
         </div>
