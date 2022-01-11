e569b702ab9517ffddcab170603940928e29205d
robbychristian <robbychristiandeleon@gmail.com>
2022-01-11 20:11:04 +0800

    Update viewactivitylog.blade.php

--- resources/views/features/viewactivitylog.blade.php
+++ resources/views/features/viewactivitylog.blade.php
@@ -11,12 +11,15 @@
             <div class="card-body">
                 @foreach ($audits as $audit)
                     <ul class="list-unstyled">
-
-                        <li>On {{ $audit->created_at }}, {{ $audit->first_name }} {{ $audit->last_name }}[{{ $audit->ip_address }}] {{ $audit->event }} this record via {{ $audit->url }}/{{ $audit->auditable_id }}?
+                        <!-- format: On 2022-01-11 17:46:18, Tine Manabs[111.125.109.69] created this record via https://kabisigapp.com/user/vulnerabilitymap/9? -->
+                        <li>On {{ $audit->created_at }}, {{ $audit->first_name }}
+                            {{ $audit->last_name }}[{{ $audit->ip_address }}] <strong>{{ $audit->event }}</strong>
+                            this record via {{ $audit->url }}/{{ $audit->auditable_id }}?
                             <ul>
-                                <li>are unaffected by this style</li>
-                                <li>will still show a bullet</li>
-                                <li>and have appropriate left margin</li>
+                                @foreach ($audit->new_values as $attribute => $values)
+                                <li>{{ $attribute }}: {{ $values }}</li>
+                                @endforeach
+                               
                             </ul>
                         </li>
 
