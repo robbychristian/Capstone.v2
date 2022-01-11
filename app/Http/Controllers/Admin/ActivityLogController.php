52e8f0b04f6b11b04e1ec116ff1df5fa2a768e7c
robbychristian <robbychristiandeleon@gmail.com>
2022-01-11 18:21:10 +0800

    Update ActivityLogController.php

--- app/Http/Controllers/Admin/ActivityLogController.php
+++ app/Http/Controllers/Admin/ActivityLogController.php
@@ -35,13 +35,11 @@ class ActivityLogController extends Controller
                 })
 
                 ->addColumn('user', function ($row) {
-                    return '<div class="media">
-                    <img class="mr-3 float-left rounded-circle" width="60" height="60" src="' . \URL::asset('KabisigGit/storage/app/public/profile_pics/' . $row->id . '/' . $row->profile_pic) . '">
-                    <div class="media-body">
-                      <h6>' . $row->first_name . '' . $row->last_name . '</h6>
-                      <small class="text-muted">' . $row->email . '</small>
-                    </div>
-                  </div>';
+                    return ' <div class= "name">
+                    <h6>' . $row->first_name . '' . $row->last_name . '</h6>
+                    <small class="text-muted">' . $row->user_email . '</small>
+                    </div>';
+                    
                 })
 
                 ->editColumn('created_at', function ($row) {
