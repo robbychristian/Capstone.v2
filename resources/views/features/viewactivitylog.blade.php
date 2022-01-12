660f6d685e07878d63639ef94b12c497b27ea6e5
robbychristian <robbychristiandeleon@gmail.com>
2022-01-12 14:50:46 +0800

    Update viewactivitylog.blade.php

--- resources/views/features/viewactivitylog.blade.php
+++ resources/views/features/viewactivitylog.blade.php
@@ -24,8 +24,8 @@
                             <table class="table table-sm table-borderless" style="color:#464646;" width="50%">
                                 <thead>
                                     <tr>
-                                        <th scope="col" style="color:#464646;" class="col-sm-4">Attribute</th>
-                                        <th scope="col" style="color:#464646;" class="col-sm-8">Value</th>
+                                        <th scope="col" style="color:#464646;" class="col-sm-3 text-center">Attribute</th>
+                                        <th scope="col" style="color:#464646;" class="col-sm-9 text-center">Value</th>
                                     </tr>
                                 </thead>
                                 <tbody>
