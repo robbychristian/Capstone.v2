69c8bd92dcc919295bc8a1530c4523ce8f4dae5c
robbychristian <robbychristiandeleon@gmail.com>
2022-01-12 20:33:08 +0800

    test

--- resources/views/features/admindashboardindex.blade.php
+++ resources/views/features/admindashboardindex.blade.php
@@ -17,10 +17,19 @@
                             <h5 class="card-title">{{ $barangay->brgy_loc }}</h5>
                             <p class="card-text">View the Disaster Statistics Report of {{ $barangay->brgy_loc }}
                             </p>
-                            <a href="/admin/dashboard/brgy/{{ $barangay->brgy_loc }}" class="btn btn-primary"><i
-                                    class="fas fa-search"></i>
-                                View
-                            </a>
+
+                            @if (Auth::user()->user_role == 1)
+                                <a href="/admin/dashboard/brgy/{{ $barangay->brgy_loc }}" class="btn btn-primary"><i
+                                        class="fas fa-search"></i>
+                                    View
+                                </a>
+                            @elseif (Auth::user()->user_role == 7)
+                                <a href="/user/dashboard-lgu/brgy/{{ $barangay->brgy_loc }}" class="btn btn-primary"><i
+                                        class="fas fa-search"></i>
+                                    View
+                                </a>
+                            @endif
+
                         </div>
                     </div>
                 </div>
