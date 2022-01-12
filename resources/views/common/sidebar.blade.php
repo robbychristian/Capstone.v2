b75885fbc8e7fc063dcd568267ec61bf192a427e
robbychristian <robbychristiandeleon@gmail.com>
2022-01-12 20:01:54 +0800

    Update sidebar.blade.php

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -57,7 +57,7 @@
         </li>
 
 
-    @elseif (Auth::user()->user_role >= 3)
+    @elseif (Auth::user()->user_role >= 3 || Auth::user()->user_role <= 6)
 
         @if (Auth::user()->user_role >= 4)
             <!-- Nav Item - Dashboard -->
@@ -66,34 +66,22 @@
                     <i class="fas fa-fw fa-chart-bar"></i>
                     <span>Dashboard</span></a>
             </li>
-
-            @if (Auth::user()->user_role == 7)
-                <!-- Nav Item - Dashboard -->
-                <li class="nav-item ">
-                    <a class="nav-link" href="#">
-                        <i class="fas fa-fw fa-chart-bar"></i>
-                        <span>Dashboard</span></a>
-                </li>
-
-            @endif
         @endif
 
 
-        @if (Auth::user()->user_role <= 6)
-            <!-- Nav Item - Account -->
-            <li class="nav-item">
-                <a class="nav-link" href="/user/account/{{ Auth::user()->id }}/edit">
-                    <i class="fas fa-fw fa-user-circle"></i>
-                    <span>Account</span></a>
-            </li>
+        <!-- Nav Item - Account -->
+        <li class="nav-item">
+            <a class="nav-link" href="/user/account/{{ Auth::user()->id }}/edit">
+                <i class="fas fa-fw fa-user-circle"></i>
+                <span>Account</span></a>
+        </li>
 
-            <!-- Nav Item - Announcements -->
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('user.announcements.index') }}">
-                    <i class="fas fa-fw fa-bullhorn"></i>
-                    <span>Announcements</span></a>
-            </li>
-        @endif
+        <!-- Nav Item - Announcements -->
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('user.announcements.index') }}">
+                <i class="fas fa-fw fa-bullhorn"></i>
+                <span>Announcements</span></a>
+        </li>
 
         <!-- Nav Item - Pages Collapse Menu -->
         <li class="nav-item">
@@ -118,40 +106,36 @@
                 <span>Vulnerability Map</span></a>
         </li>
 
-        @if (Auth::user()->user_role <= 6)
-
-
+        <!-- Nav Item - Reports -->
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('user.reports.index') }}">
+                <i class="fas fa-fw fa-edit"></i>
+                <span>Reports</span></a>
+        </li>
+        @if (Auth::user()->user_role >= 4)
             <!-- Nav Item - Reports -->
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('user.reports.index') }}">
-                    <i class="fas fa-fw fa-edit"></i>
-                    <span>Reports</span></a>
+                <a class="nav-link" href="{{ route('user.emergencymessage.create') }}">
+                    <i class="fas fa-fw fa-envelope"></i>
+                    <span>Emergency Alert
+                        Message</span></a>
             </li>
-            @if (Auth::user()->user_role >= 4)
-                <!-- Nav Item - Reports -->
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('user.emergencymessage.create') }}">
-                        <i class="fas fa-fw fa-envelope"></i>
-                        <span>Emergency Alert
-                            Message</span></a>
-                </li>
-
-                <!-- Nav Item - Reports -->
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('user.stats.index') }}">
-                        <i class="fas fa-fw fa-clipboard"></i>
-                        <span>Disaster Statistics</span></a>
-                </li>
-            @endif
-
 
             <!-- Nav Item - Reports -->
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('user.manageresident.index') }}">
-                    <i class="fas fa-fw fa-users"></i>
-                    <span>Manage Resident</span></a>
+                <a class="nav-link" href="{{ route('user.stats.index') }}">
+                    <i class="fas fa-fw fa-clipboard"></i>
+                    <span>Disaster Statistics</span></a>
             </li>
         @endif
+
+
+        <!-- Nav Item - Reports -->
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('user.manageresident.index') }}">
+                <i class="fas fa-fw fa-users"></i>
+                <span>Manage Resident</span></a>
+        </li>
 
 
     @elseif (Auth::user()->user_role === 1)
@@ -241,7 +225,6 @@
                 <i class="fas fa-fw fa-list"></i>
                 <span>Audit Logs</span></a>
         </li>
-
     @endif
 
     <!-- Divider 
