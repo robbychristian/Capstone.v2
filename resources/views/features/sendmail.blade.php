03e5978eae3d8b754890aff7b5c446bf245f3c56
robbychristian <robbychristiandeleon@gmail.com>
2022-01-13 14:37:48 +0800

    Update sendmail.blade.php

--- resources/views/features/sendmail.blade.php
+++ resources/views/features/sendmail.blade.php
@@ -8,36 +8,35 @@
             <h1 class="h3 mb-0 text-gray-800">Send Report</h1>
         </div>
 
-        <div class="container">
-            <div class="card">
-                <div class="card-body">
-                    <form action="" method="POST" enctype="multipart/form-data">
-                        <div class="form-group">
-                            <label>Email address</label>
-                            <input type="email" class="form-control" id="" aria-describedby="emailHelp"
-                                value="{{ old('email') }}">
-                            @error('email')
-                                <small class="text-danger">
-                                    {{ $message }}
-                                </small>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="">File Upload</label>
-                            <input name="file" class="form-control" type="file" id="formFile">
-                            @error('file')
-                                <small class="text-danger">
-                                    {{ $message }}
-                                </small>
-                            @enderror
-                        </div>
 
-                        <a class="btn btn-outline-secondary mr-2" href="{{ route('user.dashboard-lgu.index') }}"
-                            role="button">Cancel</a>
+        <div class="card">
+            <div class="card-body">
+                <form action="" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label>Email address</label>
+                        <input type="email" class="form-control" id="" aria-describedby="emailHelp"
+                            value="{{ old('email') }}">
+                        @error('email')
+                            <small class="text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="">File Upload</label>
+                        <input name="file" class="form-control" type="file" id="formFile">
+                        @error('file')
+                            <small class="text-danger">
+                                {{ $message }}
+                            </small>
+                        @enderror
+                    </div>
 
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+                    <a class="btn btn-outline-secondary mr-2" href="{{ route('user.dashboard-lgu.index') }}"
+                        role="button">Cancel</a>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
 
