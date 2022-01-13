f2054a65ccd95859d17104aa4efe3fa9d37e95a3
robbychristian <robbychristiandeleon@gmail.com>
2022-01-13 17:38:16 +0800

    attachments migration

---
+++ app/Models/Attachments.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class Attachments extends Model implements Auditable
+{
+    use HasFactory;
+
+    use \OwenIt\Auditing\Auditable;
+    use SoftDeletes;
+
+    protected $table = 'announcements';
+
+    protected $fillable = [
+        'user_id',
+        'email',
+        'subject',
+        'body',
+        'file',
+    ];
+    
+
+}
