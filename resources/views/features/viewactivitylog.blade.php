b00a7463b0caa24264f1e42963ded948caf9c063
robbychristian <robbychristiandeleon@gmail.com>
2022-01-19 01:45:39 +0800

    Update viewactivitylog.blade.php

--- resources/views/features/viewactivitylog.blade.php
+++ resources/views/features/viewactivitylog.blade.php
@@ -69,23 +69,53 @@
 
 
                     @elseif ($audit->event == 'updated')
-                        <div class="changed-values">
-                            @foreach (json_decode($audit->old_values) as $attribute => $value)
-                                @if ($value == null)
-                                    <span style="font-size: 20px">&#9675;</span> The {{ $attribute }} has been modified
-                                    from
-                                    <strong><em>NULL</em></strong>
-                                @else
-                                    <span style="font-size: 20px">&#9675;</span> The {{ $attribute }} has been modified
-                                    from
-                                    <strong>{{ $value }}</strong>
-                                @endif
-                            @endforeach
 
-                            @foreach (json_decode($audit->new_values) as $attribute => $value)
-                                to <strong>{{ $value }}</strong>. <br>
-                            @endforeach
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <table class="table table-borderless">
+                                    <tbody>
+
+                                        @foreach (json_decode($audit->old_values) as $attribute => $value)
+                                            <tr>
+                                                @if ($value == null)
+                                                    <td><span style="font-size: 20px">&#9675;</span> The
+                                                        {{ $attribute }}
+                                                        has
+                                                        been modified
+                                                        from
+                                                        <strong><em>NULL</em></strong>
+                                                    </td>
+                                                @else
+                                                    <td><span style="font-size: 20px">&#9675;</span> The
+                                                        {{ $attribute }}
+                                                        has
+                                                        been modified
+                                                        from
+                                                        <strong>{{ $value }}</strong>
+                                                    </td>
+                                                @endif
+                                            </tr>
+                                        @endforeach
+
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="col-sm-6">
+                                <table class="table table-borderless">
+                                    <tbody>
+                                        @foreach (json_decode($audit->new_values) as $attribute => $value)
+                                            <tr>
+                                                <td> to <strong>{{ $value }}</strong>. <br></td>
+                                            </tr>
+                                        @endforeach
+
+
+
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
+
 
                     @endif
 
