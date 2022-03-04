473080fbc525e5c944bfbff4642c9403f32dc9a4
robbychristian <robbychristiandeleon@gmail.com>
2022-03-05 04:39:36 +0800

    Update ManageResidentController.php

--- app/Http/Controllers/Auth/ManageResidentController.php
+++ app/Http/Controllers/Auth/ManageResidentController.php
@@ -28,6 +28,7 @@ class ManageResidentController extends Controller
                 ->select('users.*', 'user_profiles.*')
                 ->where('users.brgy_loc', Auth::user()->brgy_loc)
                 ->where('users.user_role', 2)
+                ->orWhere('users.user_role', 3)
                 ->get();
             return DataTables::of($data)
                 ->addIndexColumn()
