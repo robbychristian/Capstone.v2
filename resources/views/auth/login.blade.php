e1ad59e847253ab2bd50546766815d6fc2280b7e
robbychristian <robbychristiandeleon@gmail.com>
2022-03-18 04:43:19 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -96,7 +96,7 @@
     </footer>
     <!-- Footer -->
     <script type="module">
-        import express from '../../../node_modules/moment/moment.d.ts';
+        import express from '/domains/kabisigapp.com/public_html/KabisigGit/node_modules/moment/moment.js';
         console.log('LOGIN PAGE')
         console.log(moment('1995-11-11').format('MM-DD-YYYY'))
     </script>
